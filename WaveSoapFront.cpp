35318fd9d79024630f647be0344a24ee2e2ba380
Alexander Grigoriev <alegr@earthlink.net>
2000-05-31 08:07:47 -0700

    Changed WaveSoapFront.cpp, WaveSoapFront.h, WaveSoapFront.rc, resource.h
    Added current directory save/restore.
    Added color store in the profile.
    Overloaded MultiDocTemplate, to call OnNewDocument first,
    then create the view.

--- WaveSoapFront.cpp
+++ WaveSoapFront.cpp
@@ -8,7 +8,7 @@
 #include "ChildFrm.h"
 #include "WaveSoapFrontDoc.h"
 #include "WaveSoapFrontView.h"
-
+#include <float.h>
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
@@ -64,19 +64,50 @@ BOOL CWaveSoapFrontApp::InitInstance()
 	// Change the registry key under which our settings are stored.
 	// TODO: You should modify this string to be something appropriate
 	// such as the name of your company or organization.
-	SetRegistryKey(_T("Local AppWizard-Generated Applications"));
+	SetRegistryKey(_T("AleGr SoftWare"));
+
+	Profile.AddItem(_T("Settings"), _T("CurrentDir"), m_CurrentDir, _T("."));
+	if (m_CurrentDir != ".")
+	{
+		SetCurrentDirectory(m_CurrentDir);
+	}
+
+	Profile.AddItem(_T("Settings\\Colors"), _T("WaveBackground"), m_WaveBackground,
+					RGB(0xFF, 0xFF, 0xFF));
+	Profile.AddItem(_T("Settings\\Colors"), _T("SelectedWaveBackground"), m_SelectedWaveBackground,
+					RGB(0x00, 0x00, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("WaveColor"), m_WaveColor,
+					RGB(0x00, 72, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("SelectedWaveColor"), m_SelectedWaveColor,
+					RGB(0x00, 193, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("ZeroLineColor"), m_ZeroLineColor,
+					RGB(0x00, 0x00, 240));
+	Profile.AddItem(_T("Settings\\Colors"), _T("SelectedZeroLineColor"), m_SelectedZeroLineColor,
+					RGB(0x00, 0x00, 240));
+	Profile.AddItem(_T("Settings\\Colors"), _T("6dBLineColor"), m_6dBLineColor,
+					RGB(192, 192, 192));
+	Profile.AddItem(_T("Settings\\Colors"), _T("Selected6dBLineColor"), m_Selected6dBLineColor,
+					RGB(192, 192, 192));
+	Profile.AddItem(_T("Settings\\Colors"), _T("ChannelSeparatorColor"), m_ChannelSeparatorColor,
+					RGB(0x00, 0x00, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("m_SelectedChannelSeparatorColor"), m_SelectedChannelSeparatorColor,
+					RGB(0x00, 0x00, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("InterpolatedColor"), m_InterpolatedColor,
+					RGB(0x00, 0x00, 0x00));
+	Profile.AddItem(_T("Settings\\Colors"), _T("SelectedInterpolatedColor"), m_SelectedInterpolatedColor,
+					RGB(0x00, 0x00, 0x00));
 
 	LoadStdProfileSettings();  // Load standard INI file options (including MRU)
 
 	// Register the application's document templates.  Document templates
 	//  serve as the connection between documents, frame windows and views.
 
-	CMultiDocTemplate* pDocTemplate;
-	pDocTemplate = new CMultiDocTemplate(
-										IDR_WAVESOTYPE,
-										RUNTIME_CLASS(CWaveSoapFrontDoc),
-										RUNTIME_CLASS(CChildFrame), // custom MDI child frame
-										RUNTIME_CLASS(CWaveSoapFrontView));
+	CWaveSoapDocTemplate* pDocTemplate;
+	pDocTemplate = new CWaveSoapDocTemplate(
+											IDR_WAVESOTYPE,
+											RUNTIME_CLASS(CWaveSoapFrontDoc),
+											RUNTIME_CLASS(CChildFrame), // custom MDI child frame
+											RUNTIME_CLASS(CWaveSoapFrontView));
 	AddDocTemplate(pDocTemplate);
 
 	// create main MDI Frame window
@@ -87,6 +118,7 @@ BOOL CWaveSoapFrontApp::InitInstance()
 
 	// Parse command line for standard shell commands, DDE, file open
 	CCommandLineInfo cmdInfo;
+	cmdInfo.m_nShellCommand = CCommandLineInfo::FileNothing;
 	ParseCommandLine(cmdInfo);
 
 	// Dispatch commands specified on the command line
@@ -154,6 +186,115 @@ void CWaveSoapFrontApp::OnAppAbout()
 	aboutDlg.DoModal();
 }
 
+double CWaveSoapFrontApp::GetProfileDouble(LPCTSTR Section, LPCTSTR ValueName,
+											double Default, double MinVal, double MaxVal)
+{
+	CString s = GetProfileString(Section, ValueName, "");
+	double val;
+	TCHAR * endptr;
+
+	if (s.IsEmpty()
+		|| (val = _tcstod(s, & endptr), 0 != *endptr)
+		|| _isnan(val)
+		|| ! _finite(val))
+	{
+		val = Default;
+	}
+
+	if (val < MinVal) val = MinVal;
+	if (val > MaxVal) val = MaxVal;
+
+	return val;
+}
+
+void CWaveSoapFrontApp::WriteProfileDouble(LPCTSTR Section, LPCTSTR ValueName,
+											double value)
+{
+	CString s;
+	s.Format("%g", value);
+	WriteProfileString(Section, ValueName, s);
+}
+
+CDocument* CWaveSoapDocTemplate::OpenDocumentFile(LPCTSTR lpszPathName,
+												BOOL bMakeVisible)
+{
+	CDocument* pDocument = CreateNewDocument();
+	if (pDocument == NULL)
+	{
+		TRACE0("CDocTemplate::CreateNewDocument returned NULL.\n");
+		AfxMessageBox(AFX_IDP_FAILED_TO_CREATE_DOC);
+		return NULL;
+	}
+	ASSERT_VALID(pDocument);
+	// modified version of original CMultiDocTemplate::OpenDocumentFile
+	// Document is created and initialized first, then the frame is created
+	if (lpszPathName == NULL)
+	{
+		// create a new document - with default document name
+		SetDefaultTitle(pDocument);
+
+		// avoid creating temporary compound file when starting up invisible
+		if (!bMakeVisible)
+			pDocument->m_bEmbedded = TRUE;
+
+		if (!pDocument->OnNewDocument())
+		{
+			// user has be alerted to what failed in OnNewDocument
+			TRACE0("CDocument::OnNewDocument returned FALSE.\n");
+			//pFrame->DestroyWindow();
+			delete pDocument;       // explicit delete on error
+			return NULL;
+		}
+
+		// it worked, now bump untitled count
+		m_nUntitledCount++;
+	}
+	else
+	{
+		// open an existing document
+		CWaitCursor wait;
+		if (!pDocument->OnOpenDocument(lpszPathName))
+		{
+			// user has be alerted to what failed in OnOpenDocument
+			TRACE0("CDocument::OnOpenDocument returned FALSE.\n");
+			//pFrame->DestroyWindow();
+			delete pDocument;       // explicit delete on error
+			return NULL;
+		}
+		pDocument->SetPathName(lpszPathName);
+	}
+
+	BOOL bAutoDelete = pDocument->m_bAutoDelete;
+	pDocument->m_bAutoDelete = FALSE;   // don't destroy if something goes wrong
+	CFrameWnd* pFrame = CreateNewFrame(pDocument, NULL);
+	pDocument->m_bAutoDelete = bAutoDelete;
+	if (pFrame == NULL)
+	{
+		AfxMessageBox(AFX_IDP_FAILED_TO_CREATE_DOC);
+		delete pDocument;       // explicit delete on error
+		return NULL;
+	}
+	ASSERT_VALID(pFrame);
+
+	InitialUpdateFrame(pFrame, pDocument, bMakeVisible);
+	return pDocument;
+}
+
 /////////////////////////////////////////////////////////////////////////////
 // CWaveSoapFrontApp message handlers
 
+
+int CWaveSoapFrontApp::ExitInstance()
+{
+	// TODO: Add your specialized code here and/or call the base class
+	LPTSTR dirbuf = m_CurrentDir.GetBuffer(MAX_PATH+1);
+	if (dirbuf)
+	{
+		GetCurrentDirectory(MAX_PATH+1, dirbuf);
+		m_CurrentDir.ReleaseBuffer();
+	}
+	Profile.UnloadSection(_T("Settings"));
+	Profile.UnloadSection(_T("Settings\\Colors"));
+
+	return CWinApp::ExitInstance();
+}
--- WaveSoapFront.h
+++ WaveSoapFront.h
@@ -14,6 +14,7 @@
 
 #include "resource.h"       // main symbols
 
+#include "ApplicationProfile.h"
 /////////////////////////////////////////////////////////////////////////////
 // CWaveSoapFrontApp:
 // See WaveSoapFront.cpp for the implementation of this class
@@ -29,9 +30,30 @@ public:
 	//{{AFX_VIRTUAL(CWaveSoapFrontApp)
 public:
 	virtual BOOL InitInstance();
+	virtual int ExitInstance();
 	//}}AFX_VIRTUAL
 
 // Implementation
+	double GetProfileDouble(LPCTSTR Section, LPCTSTR ValueName,
+							double Default, double MinVal, double MaxVal);
+	void WriteProfileDouble(LPCTSTR Section, LPCTSTR ValueName, double value);
+	CApplicationProfile Profile;
+	CString m_CurrentDir;
+	// display colors:
+	DWORD m_WaveBackground;
+	DWORD m_SelectedWaveBackground;
+	DWORD m_WaveColor;
+	DWORD m_6dBLineColor;
+	DWORD m_ZeroLineColor;
+	DWORD m_ChannelSeparatorColor;
+	DWORD m_InterpolatedColor;
+	DWORD m_SelectedWaveColor;
+	DWORD m_Selected6dBLineColor;
+	DWORD m_SelectedZeroLineColor;
+	DWORD m_SelectedChannelSeparatorColor;
+	DWORD m_SelectedInterpolatedColor;
+
+
 	//{{AFX_MSG(CWaveSoapFrontApp)
 	afx_msg void OnAppAbout();
 	// NOTE - the ClassWizard will add and remove member functions here.
@@ -40,10 +62,21 @@ public:
 	DECLARE_MESSAGE_MAP()
 };
 
-
+typedef CWaveSoapFrontApp CWaveSoapApp;
 /////////////////////////////////////////////////////////////////////////////
+class CWaveSoapDocTemplate : public CMultiDocTemplate
+{
+public:
+	CWaveSoapDocTemplate( UINT nIDResource, CRuntimeClass* pDocClass,
+						CRuntimeClass* pFrameClass, CRuntimeClass* pViewClass )
+		:CMultiDocTemplate(nIDResource, pDocClass, pFrameClass, pViewClass)
+	{
+	}
+	~CWaveSoapDocTemplate() {}
+	virtual CDocument* OpenDocumentFile( LPCTSTR lpszPathName, BOOL bMakeVisible = TRUE );
+
+};
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
 #endif // !defined(AFX_WAVESOAPFRONT_H__FFA16C44_2FA7_11D4_9ADD_00C0F0583C4B__INCLUDED_)
