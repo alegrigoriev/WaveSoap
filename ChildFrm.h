28ae357273dfa3c315b31687b77a167be736326c
Alexander Grigoriev <alegr@earthlink.net>
2000-06-08 22:18:07 -0700

    Added client window class, which creates TimeRulerView and the main view

--- ChildFrm.h
+++ ChildFrm.h
@@ -9,6 +9,41 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+/////////////////////////////////////////////////////////////////////////////
+// CWaveMDIChildClient window
+
+class CWaveMDIChildClient : public CWnd
+{
+// Construction
+public:
+	CWaveMDIChildClient();
+
+// Attributes
+public:
+
+// Operations
+public:
+	CWnd * CreateView(CRuntimeClass* pViewClass,
+					CRect rect, int nID, CCreateContext* pContext);
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CWaveMDIChildClient)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	virtual ~CWaveMDIChildClient();
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CWaveMDIChildClient)
+	// NOTE - the ClassWizard will add and remove member functions here.
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
 
 class CChildFrame : public CMDIChildWnd
 {
@@ -18,7 +53,7 @@ public:
 
 // Attributes
 public:
-
+	CWaveMDIChildClient m_wClient;
 // Operations
 public:
 
@@ -28,6 +63,9 @@ public:
 public:
 	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
 	virtual void ActivateFrame(int nCmdShow);
+	virtual void RecalcLayout(BOOL bNotify = TRUE);
+protected:
+	virtual BOOL OnCreateClient(LPCREATESTRUCT lpcs, CCreateContext* pContext);
 	//}}AFX_VIRTUAL
 
 // Implementation
@@ -41,14 +79,14 @@ public:
 // Generated message map functions
 protected:
 	//{{AFX_MSG(CChildFrame)
-	// NOTE - the ClassWizard will add and remove member functions here.
-	//    DO NOT EDIT what you see in these blocks of generated code!
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
 
 /////////////////////////////////////////////////////////////////////////////
 
+
+/////////////////////////////////////////////////////////////////////////////
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
