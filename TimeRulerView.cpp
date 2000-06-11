2289d490de72797fc2eaa944612a5bda62588070
Alexander Grigoriev <alegr@earthlink.net>
2000-06-11 07:42:00 -0700

    Changed TimeRulerView.cpp, TimeRulerView.h
    Implemented 3 display modes: samples,
    HH:MM:SS, seconds. Implemented context menu for mode switching.

--- TimeRulerView.cpp
+++ TimeRulerView.cpp
@@ -17,6 +17,7 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNCREATE(CTimeRulerView, CHorizontalRuler)
 
 CTimeRulerView::CTimeRulerView()
+	: m_CurrentDisplayMode(ShowSamples)
 {
 }
 
@@ -27,6 +28,12 @@ CTimeRulerView::~CTimeRulerView()
 
 BEGIN_MESSAGE_MAP(CTimeRulerView, CHorizontalRuler)
 	//{{AFX_MSG_MAP(CTimeRulerView)
+	ON_COMMAND(IDC_VIEW_RULER_HHMMSS, OnViewRulerHhmmss)
+	ON_UPDATE_COMMAND_UI(IDC_VIEW_RULER_HHMMSS, OnUpdateViewRulerHhmmss)
+	ON_COMMAND(IDC_VIEW_RULER_SAMPLES, OnViewRulerSamples)
+	ON_UPDATE_COMMAND_UI(IDC_VIEW_RULER_SAMPLES, OnUpdateViewRulerSamples)
+	ON_COMMAND(IDC_VIEW_RULER_SECONDS, OnViewRulerSeconds)
+	ON_UPDATE_COMMAND_UI(IDC_VIEW_RULER_SECONDS, OnUpdateViewRulerSeconds)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -35,7 +42,24 @@ END_MESSAGE_MAP()
 
 void CTimeRulerView::OnDraw(CDC* pDC)
 {
-	CDocument* pDoc = GetDocument();
+	switch (m_CurrentDisplayMode)
+	{
+	case ShowSamples:
+		DrawRulerSamples(pDC);
+		break;
+	case ShowHhMmSs:
+		DrawRulerHhMmSs(pDC);
+		break;
+	case ShowSeconds:
+		DrawRulerSeconds(pDC);
+	default:
+		break;
+	}
+}
+
+void CTimeRulerView::DrawRulerSamples(CDC* pDC)
+{
+	CWaveSoapFrontDoc* pDoc = GetDocument();
 
 	// background is erased by gray brush.
 	// draw horizontal line with ticks and numbers
@@ -45,16 +69,19 @@ void CTimeRulerView::OnDraw(CDC* pDC)
 	GetClientRect( & cr);
 
 	// calculate position string length
-	int nLength = pDC->GetTextExtent("0000000000", 10).cx;
+	CString s;
+	int nLength;
+	int dist, nTickDist;
+	unsigned nSamples, k;
+	nLength = pDC->GetTextExtent("0000000000", 10).cx;
+	nSamples = 1.5 * nLength / GetXScaleDev();
 	// calculate how much samples can be between the numbers
-	unsigned nSamples = 1.5 * nLength / GetXScaleDev();
 	if (nSamples > INT_MAX / 10)
 	{
 		nSamples = INT_MAX / 10;
 	}
 	// find the closest bigger 1,2,5 * 10^x
-	int dist, nTickDist;
-	for (unsigned k = 1; k <= INT_MAX / 10; k *= 10)
+	for (k = 1; k <= INT_MAX / 10; k *= 10)
 	{
 		dist = k;
 		nTickDist = k;
@@ -99,6 +126,10 @@ void CTimeRulerView::OnDraw(CDC* pDC)
 
 	for(;; nFirstSample += nTickDist)
 	{
+		if (nFirstSample > pDoc->WaveFileSamples())
+		{
+			break;
+		}
 		int x = WorldToWindowX(nFirstSample);
 		if (x < cr.left)
 		{
@@ -139,6 +170,380 @@ void CTimeRulerView::OnDraw(CDC* pDC)
 	pDC->SelectObject(pOldFont);
 }
 
+void CTimeRulerView::DrawRulerHhMmSs(CDC* pDC)
+{
+	CWaveSoapFrontDoc* pDoc = GetDocument();
+
+	// background is erased by gray brush.
+	// draw horizontal line with ticks and numbers
+	CGdiObject * pOldFont = (CFont *) pDC->SelectStockObject(ANSI_VAR_FONT);
+	CPen DarkGrayPen(PS_SOLID, 0, 0x808080);
+	CRect cr;
+	GetClientRect( & cr);
+
+	// calculate position string length
+	int nTickCount;
+	float SampleRate = pDoc->m_WavFile.SampleRate();
+
+	int nLength = pDC->GetTextExtent("00:00:00.0000", 13).cx;
+	double DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
+	// select distance between ticks
+	double multiplier = 1.;
+	double divisor = 1.;
+	if (DistTime >= 1.)
+	{
+		if (DistTime > 3600.)
+		{
+			multiplier = 3600.;
+			DistTime = ceil(DistTime / 3600.);
+		}
+		else if (DistTime > 60.)
+		{
+			multiplier = 60.;
+			DistTime = ceil(DistTime / 60.);
+		}
+		else
+		{
+			DistTime = ceil(DistTime);
+		}
+		// find the closest bigger 1,2,5, 10, 20, 30
+		if (DistTime <= 1.)
+		{
+			DistTime = 1.;
+			nTickCount = 10;
+		}
+		else if (DistTime <= 2.)
+		{
+			DistTime = 2.;
+			nTickCount = 2;
+		}
+		else if (DistTime <= 5.)
+		{
+			DistTime = 5.;
+			nTickCount = 5.;
+		}
+		else if (DistTime <= 10.)
+		{
+			DistTime = 10.;
+			nTickCount = 10.;
+		}
+		else if (DistTime <= 20.)
+		{
+			DistTime = 20.;
+			nTickCount = 2.;
+		}
+		else if (DistTime <= 30.)
+		{
+			DistTime = 30.;
+			nTickCount = 3.;
+		}
+		else
+		{
+			DistTime = 60.;
+			nTickCount = 6.;
+		}
+	}
+	else    // DistTime < 1
+	{
+		divisor = 1000.;
+		DistTime = ceil(DistTime * 1000.);
+		// find the closest bigger 1,2,5 * 10^x
+		for (; ; multiplier *= 10., DistTime = ceil(DistTime / 10.))
+		{
+			if (DistTime <= 1.)
+			{
+				DistTime = 1.;
+				nTickCount = 10;
+				break;
+			}
+			else if (DistTime <= 2.)
+			{
+				DistTime = 2.;
+				nTickCount = 2;
+				break;
+			}
+			else if (DistTime <= 5.)
+			{
+				DistTime = 5.;
+				nTickCount = 5.;
+				break;
+			}
+		}
+	}
+
+
+	DistTime = DistTime * multiplier / divisor;
+
+	double nFirstTime = floor(WindowToWorldX(cr.left) / (SampleRate * DistTime))
+						* DistTime;
+
+	pDC->SetTextAlign(TA_BOTTOM | TA_LEFT);
+	pDC->SetTextColor(0x000000);   // black
+	pDC->SetBkMode(TRANSPARENT);
+
+	CGdiObject * pOldPen = pDC->SelectStockObject(BLACK_PEN);
+	pDC->MoveTo(cr.left, cr.bottom - 5);
+	pDC->LineTo(cr.right, cr.bottom - 5);
+
+	pDC->SelectStockObject(WHITE_PEN);
+	pDC->MoveTo(cr.left, cr.bottom - 4);
+	pDC->LineTo(cr.right, cr.bottom - 4);
+
+	pDC->SelectObject( & DarkGrayPen);
+	pDC->MoveTo(cr.left, cr.bottom - 6);
+	pDC->LineTo(cr.right, cr.bottom - 6);
+
+	TCHAR TimeSeparator[] = _T(":");
+	TCHAR DecimalPoint[] = _T(".");
+	GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SDECIMAL, DecimalPoint, 1);
+	GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_STIME, TimeSeparator, 2);
+
+	for(int nTick = 0; ; nTick++)
+	{
+		double time = nFirstTime + DistTime
+					* nTick / double(nTickCount);
+
+		double sample = time * SampleRate;
+		if (sample > pDoc->WaveFileSamples())
+		{
+			break;
+		}
+
+		int x = WorldToWindowX(sample);
+		if (x < cr.left)
+		{
+			continue;
+		}
+		if (x > cr.right)
+		{
+			break;
+		}
+		pDC->SelectStockObject(BLACK_PEN);
+		pDC->MoveTo(x - 1, cr.bottom - 6);
+
+		if (0 == nTick % nTickCount)
+		{
+			// draw bigger tick (6 pixels high) and the number
+			pDC->LineTo(x - 1, cr.bottom - 12);
+			CString s;
+
+			time += 0.0005;
+			int hh = time / 3600.;
+			time -= hh * 3600;
+			int mm = time / 60.;
+			time -= mm * 60;
+			int ss = time;
+			time -= ss;
+			int ms = time * 1000.;
+
+			if (DistTime < 1.)
+			{
+				if (hh != 0)
+				{
+					s.Format(_T("%d%c%02d%c%02d%c%03d"),
+							hh, TimeSeparator[0],
+							mm, TimeSeparator[0],
+							ss, DecimalPoint[0],
+							ms);
+				}
+				else if (mm != 0)
+				{
+					s.Format(_T("%d%c%02d%c%03d"),
+							mm, TimeSeparator[0],
+							ss, DecimalPoint[0],
+							ms);
+				}
+				else
+				{
+					s.Format(_T("%d%c%03d"),
+							ss, DecimalPoint[0],
+							ms);
+				}
+			}
+			else
+			{
+				if (hh != 0)
+				{
+					s.Format(_T("%d%c%02d%c%02d"),
+							hh, TimeSeparator[0],
+							mm, TimeSeparator[0],
+							ss);
+				}
+				else
+				{
+					s.Format(_T("%d%c%02d"),
+							mm, TimeSeparator[0],
+							ss);
+				}
+			}
+
+			pDC->TextOut(x + 2, cr.bottom - 9, s);
+
+			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x, cr.bottom - 6);
+			pDC->LineTo(x, cr.bottom - 12);
+		}
+		else
+		{
+			// draw small tick (2 pixels high)
+			pDC->LineTo(x - 1, cr.bottom - 8);
+
+			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x, cr.bottom - 6);
+			pDC->LineTo(x, cr.bottom - 8);
+		}
+	}
+
+	pDC->SelectObject(pOldPen);
+	pDC->SelectObject(pOldFont);
+}
+
+void CTimeRulerView::DrawRulerSeconds(CDC* pDC)
+{
+	CWaveSoapFrontDoc* pDoc = GetDocument();
+
+	// background is erased by gray brush.
+	// draw horizontal line with ticks and numbers
+	CGdiObject * pOldFont = (CFont *) pDC->SelectStockObject(ANSI_VAR_FONT);
+	CPen DarkGrayPen(PS_SOLID, 0, 0x808080);
+	CRect cr;
+	GetClientRect( & cr);
+
+	// calculate position string length
+	int nTickCount;
+	float SampleRate = pDoc->m_WavFile.SampleRate();
+
+	int nLength = pDC->GetTextExtent("00000.0000", 13).cx;
+	double DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
+	// select distance between ticks
+	double multiplier = 1.;
+	double divisor = 1.;
+
+	if (DistTime >= 1.)
+	{
+		DistTime = ceil(DistTime);
+	}
+	else
+	{
+		divisor = 1000.;
+		DistTime = ceil(DistTime * 1000.);
+	}
+
+	// find the closest bigger 1,2,5 * 10^x
+	for (; ; multiplier *= 10., DistTime = ceil(DistTime / 10.))
+	{
+		if (DistTime <= 1.)
+		{
+			DistTime = 1.;
+			nTickCount = 10;
+			break;
+		}
+		else if (DistTime <= 2.)
+		{
+			DistTime = 2.;
+			nTickCount = 2;
+			break;
+		}
+		else if (DistTime <= 5.)
+		{
+			DistTime = 5.;
+			nTickCount = 5.;
+			break;
+		}
+	}
+
+	DistTime = DistTime * multiplier / divisor;
+
+	double nFirstTime = floor(WindowToWorldX(cr.left) / (SampleRate * DistTime))
+						* DistTime;
+
+	pDC->SetTextAlign(TA_BOTTOM | TA_LEFT);
+	pDC->SetTextColor(0x000000);   // black
+	pDC->SetBkMode(TRANSPARENT);
+
+	CGdiObject * pOldPen = pDC->SelectStockObject(BLACK_PEN);
+	pDC->MoveTo(cr.left, cr.bottom - 5);
+	pDC->LineTo(cr.right, cr.bottom - 5);
+
+	pDC->SelectStockObject(WHITE_PEN);
+	pDC->MoveTo(cr.left, cr.bottom - 4);
+	pDC->LineTo(cr.right, cr.bottom - 4);
+
+	pDC->SelectObject( & DarkGrayPen);
+	pDC->MoveTo(cr.left, cr.bottom - 6);
+	pDC->LineTo(cr.right, cr.bottom - 6);
+
+	TCHAR DecimalPoint[] = _T(".");
+	GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SDECIMAL, DecimalPoint, 1);
+
+	for(int nTick = 0; ; nTick++)
+	{
+		double time = nFirstTime + DistTime
+					* nTick / double(nTickCount);
+
+		double sample = time * SampleRate;
+		if (sample > pDoc->WaveFileSamples())
+		{
+			break;
+		}
+
+		int x = WorldToWindowX(sample);
+
+		if (x < cr.left)
+		{
+			continue;
+		}
+		if (x > cr.right)
+		{
+			break;
+		}
+		pDC->SelectStockObject(BLACK_PEN);
+		pDC->MoveTo(x - 1, cr.bottom - 6);
+
+		if (0 == nTick % nTickCount)
+		{
+			// draw bigger tick (6 pixels high) and the number
+			pDC->LineTo(x - 1, cr.bottom - 12);
+			CString s;
+
+			time += 0.0005;
+			int ss = time;
+			time -= ss;
+			int ms = time * 1000.;
+
+			if (DistTime < 1.)
+			{
+				s.Format(_T("%d%c%03d"),
+						ss, DecimalPoint[0],
+						ms);
+			}
+			else
+			{
+				s.Format(_T("%d%c0"),
+						ss, DecimalPoint[0]);
+			}
+
+			pDC->TextOut(x + 2, cr.bottom - 9, s);
+
+			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x, cr.bottom - 6);
+			pDC->LineTo(x, cr.bottom - 12);
+		}
+		else
+		{
+			// draw small tick (2 pixels high)
+			pDC->LineTo(x - 1, cr.bottom - 8);
+
+			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x, cr.bottom - 6);
+			pDC->LineTo(x, cr.bottom - 8);
+		}
+	}
+
+	pDC->SelectObject(pOldPen);
+	pDC->SelectObject(pOldFont);
+}
+
 /////////////////////////////////////////////////////////////////////////////
 // CTimeRulerView diagnostics
 
@@ -152,8 +557,47 @@ void CTimeRulerView::Dump(CDumpContext& dc) const
 {
 	CScaledScrollView::Dump(dc);
 }
+
+CWaveSoapFrontDoc* CTimeRulerView::GetDocument() // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CWaveSoapFrontDoc)));
+	return (CWaveSoapFrontDoc*)m_pDocument;
+}
 #endif //_DEBUG
 
 /////////////////////////////////////////////////////////////////////////////
 // CTimeRulerView message handlers
 
+
+void CTimeRulerView::OnViewRulerHhmmss()
+{
+	m_CurrentDisplayMode = ShowHhMmSs;
+	Invalidate();
+}
+
+void CTimeRulerView::OnUpdateViewRulerHhmmss(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetRadio(m_CurrentDisplayMode == ShowHhMmSs);
+}
+
+void CTimeRulerView::OnViewRulerSamples()
+{
+	m_CurrentDisplayMode = ShowSamples;
+	Invalidate();
+}
+
+void CTimeRulerView::OnUpdateViewRulerSamples(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetRadio(m_CurrentDisplayMode == ShowSamples);
+}
+
+void CTimeRulerView::OnViewRulerSeconds()
+{
+	m_CurrentDisplayMode = ShowSeconds;
+	Invalidate();
+}
+
+void CTimeRulerView::OnUpdateViewRulerSeconds(CCmdUI* pCmdUI)
+{
+	pCmdUI->SetRadio(m_CurrentDisplayMode == ShowSeconds);
+}
