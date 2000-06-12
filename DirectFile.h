9e9fa9a12dbe824d4d8c2dfb2cdc9b1d725c72cd
Alexander Grigoriev <alegr@earthlink.net>
2000-06-12 08:34:08 -0700

    Direct file support - operations and cache

---
+++ DirectFile.h
@@ -0,0 +1,20 @@
+// DirectFile.h: interface for the CDirectFile class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_DIRECTFILE_H__B7AA7401_4036_11D4_9ADD_00C0F0583C4B__INCLUDED_)
+#define AFX_DIRECTFILE_H__B7AA7401_4036_11D4_9ADD_00C0F0583C4B__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CDirectFile : public CFile
+{
+public:
+	CDirectFile();
+	virtual ~CDirectFile();
+
+};
+
+#endif // !defined(AFX_DIRECTFILE_H__B7AA7401_4036_11D4_9ADD_00C0F0583C4B__INCLUDED_)
