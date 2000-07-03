aa31738b932bb81c3d85714962d0ded378bdace1
Alexander Grigoriev <alegr@earthlink.net>
2000-07-03 10:30:13 -0700

    Mode initialization moved to OnCreate from OnInitialiUpdate

--- WaveSoapFrontView.h
+++ WaveSoapFrontView.h
@@ -93,6 +93,7 @@ protected:
 	afx_msg BOOL OnMouseWheel(UINT nFlags, short zDelta, CPoint pt);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 	afx_msg void OnKeyDown(UINT nChar, UINT nRepCnt, UINT nFlags);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
