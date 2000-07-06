ca0e711670e5cd7d59ecdcd935d96b91dc5b96b1
Alexander Grigoriev <alegr@earthlink.net>
2000-07-05 19:37:17 -0700

    Everything works

--- FftRulerView.cpp
+++ FftRulerView.cpp
@@ -14,7 +14,7 @@ static char THIS_FILE[] = __FILE__;
 /////////////////////////////////////////////////////////////////////////////
 // CFftRulerView
 
-IMPLEMENT_DYNCREATE(CFftRulerView, CView)
+IMPLEMENT_DYNCREATE(CFftRulerView, CVerticalRuler)
 
 CFftRulerView::CFftRulerView()
 {
@@ -25,19 +25,166 @@ CFftRulerView::~CFftRulerView()
 }
 
 
-BEGIN_MESSAGE_MAP(CFftRulerView, CView)
+BEGIN_MESSAGE_MAP(CFftRulerView, CVerticalRuler)
 	//{{AFX_MSG_MAP(CFftRulerView)
-		// NOTE - the ClassWizard will add and remove mapping macros here.
+	ON_WM_LBUTTONDBLCLK()
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CFftRulerView drawing
 
+static int fround(double d)
+{
+	if (d >= 0.)
+	{
+		return int(d + 0.5);
+	}
+	else
+	{
+		return int(d - 0.5);
+	}
+}
+
 void CFftRulerView::OnDraw(CDC* pDC)
 {
-	CDocument* pDoc = GetDocument();
-	// TODO: add draw code here
+	CWaveSoapFrontDoc* pDoc = GetDocument();
+
+	CWaveFftView * pMasterView = DYNAMIC_DOWNCAST(CWaveFftView, m_pVertMaster);
+	if (NULL == pMasterView)
+	{
+		return; // not attached
+	}
+
+	CRect cr;
+	GetClientRect(cr);
+
+
+	int nVertStep = GetSystemMetrics(SM_CYMENU);
+	int nChannels = pDoc->WaveChannels();
+	int nHeight = cr.Height() / nChannels;
+
+	double VerticalScale = pMasterView->m_VerticalScale;
+
+	// if all the chart was drawn, how many scans it would have:
+	int TotalRows = nHeight * VerticalScale;
+
+	if (0 == TotalRows)
+	{
+		return;
+	}
+
+	int LastFftSample = pMasterView->m_FftOrder - pMasterView->m_FirstbandVisible;
+	int FirstFftSample = LastFftSample + (-nHeight * pMasterView->m_FftOrder) / TotalRows;
+	if (FirstFftSample < 0)
+	{
+		LastFftSample -= FirstFftSample;
+		FirstFftSample = 0;
+	}
+	int FirstRowInView = FirstFftSample * TotalRows / pMasterView->m_FftOrder;
+	int FftSamplesInView = LastFftSample - FirstFftSample + 1;
+
+	CGdiObject * pOldFont = (CFont *) pDC->SelectStockObject(ANSI_VAR_FONT);
+	CGdiObject * OldPen = pDC->SelectStockObject(BLACK_PEN);
+	TEXTMETRIC tm;
+	pDC->GetTextMetrics( & tm);
+	pDC->SetTextAlign(TA_BOTTOM | TA_RIGHT);
+	pDC->SetTextColor(0x000000);   // black
+	pDC->SetBkMode(TRANSPARENT);
+
+	int nSampleUnits = nVertStep * pDoc->WaveFormat()->nSamplesPerSec / (nHeight * VerticalScale);
+	// round sample units to 10 or 5
+	int step;
+	for (step = 1; step < nSampleUnits; step *= 10)
+	{
+		if (step * 2 >= nSampleUnits)
+		{
+			step *= 2;
+			break;
+		}
+		if (step * 5 >= nSampleUnits)
+		{
+			step *= 5;
+			break;
+		}
+	}
+	double YScaleDev = pMasterView->GetYScaleDev();
+	const int ChannelSeparatorY = nHeight;
+	if (2 == nChannels)
+	{
+		pDC->MoveTo(0, ChannelSeparatorY);
+		pDC->LineTo(cr.right, ChannelSeparatorY);
+	}
+	for (int ch = 0; ch < nChannels; ch++)
+	{
+		double Offset = nHeight;
+		int ClipHigh = cr.bottom;
+		int ClipLow = cr.top;
+		if (nChannels > 1)
+		{
+			if (0 == ch)
+			{
+				ClipHigh = ChannelSeparatorY;
+			}
+			else
+			{
+				Offset = cr.Height();
+				ClipLow = ChannelSeparatorY + 1;
+			}
+		}
+		ClipLow += tm.tmHeight / 2;
+		ClipHigh -= tm.tmHeight / 2;
+		int yLow = pMasterView->m_FirstbandVisible *
+					0.5 * pDoc->WaveFormat()->nSamplesPerSec / pMasterView->m_FftOrder;
+		// round to the next multiple of step
+		yLow += (step*0x10000-yLow) % step;
+		int yHigh = yLow + 0.5 * pDoc->WaveFormat()->nSamplesPerSec / VerticalScale;
+		yHigh -= (step*0x10000+yHigh) % step;
+		ASSERT(yLow <= yHigh);
+		for (int y = yLow; y <= yHigh; y += step)
+		{
+			// y is frequency
+			double band = double(y) / (0.5 * pDoc->WaveFormat()->nSamplesPerSec)
+						* pMasterView->m_FftOrder;
+			int yDev= Offset - fround((band - pMasterView->m_FirstbandVisible)
+									/ pMasterView->m_FftOrder * nHeight * VerticalScale);
+			if (yDev - tm.tmHeight/2 < ClipLow
+				|| yDev + tm.tmHeight/2 > ClipHigh)
+			{
+				continue;
+			}
+			pDC->MoveTo(cr.right - 3, yDev);
+			pDC->LineTo(cr.right, yDev);
+			CString s;
+			s.Format(_T("%d"), y);
+
+			pDC->TextOut(cr.right - 3, yDev + tm.tmHeight / 2, s);
+		}
+	}
+	pDC->SelectObject(OldPen);
+	pDC->SelectObject(pOldFont);
+}
+
+void CFftRulerView::OnUpdate( CView* pSender, LPARAM lHint, CObject* pHint )
+{
+	if ((lHint == CWaveFftView::FFT_OFFSET_CHANGED
+			|| lHint == CWaveFftView::FFT_SCALE_CHANGED)
+		&& NULL == pHint)
+	{
+		Invalidate();
+	}
+}
+
+void CFftRulerView::OnLButtonDblClk(UINT nFlags, CPoint point)
+{
+	CVerticalRuler::OnLButtonDblClk(nFlags, point);
+	// set default scale
+	CWaveFftView * pView =
+		DYNAMIC_DOWNCAST(CWaveFftView, m_pVertMaster);
+	if (NULL != pView)
+	{
+		pView->OnViewZoomvertNormal();
+	}
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -46,12 +193,17 @@ void CFftRulerView::OnDraw(CDC* pDC)
 #ifdef _DEBUG
 void CFftRulerView::AssertValid() const
 {
-	CView::AssertValid();
+	CVerticalRuler::AssertValid();
 }
 
 void CFftRulerView::Dump(CDumpContext& dc) const
 {
-	CView::Dump(dc);
+	CVerticalRuler::Dump(dc);
+}
+CWaveSoapFrontDoc* CFftRulerView::GetDocument() // non-debug version is inline
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CWaveSoapFrontDoc)));
+	return (CWaveSoapFrontDoc*)m_pDocument;
 }
 #endif //_DEBUG
 
