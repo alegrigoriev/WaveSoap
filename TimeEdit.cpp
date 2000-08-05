0b0a7e2138d12c1367df251c273ff1dfebee7af4
Alexander Grigoriev <alegr@earthlink.net>
2000-08-04 20:20:14 -0700

    Edit time in different formats

---
+++ TimeEdit.cpp
@@ -0,0 +1,33 @@
+// TimeEdit.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "WaveSoapFront.h"
+#include "TimeEdit.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CTimeEdit
+
+CTimeEdit::CTimeEdit()
+{
+}
+
+CTimeEdit::~CTimeEdit()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CTimeEdit, CEdit)
+	//{{AFX_MSG_MAP(CTimeEdit)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CTimeEdit message handlers
