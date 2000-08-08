68ef884e1c2f6874e28a93f5a05664526183a88c
Alexander Grigoriev <alegr@earthlink.net>
2000-08-07 20:35:46 -0700

    Implemented VolumeChange, DC Offset dialogs

--- OperationDialogs.cpp
+++ OperationDialogs.cpp
@@ -121,41 +121,53 @@ void CVolumeChangeDialog::UpdateVolumeData(CDataExchange* pDX, BOOL InPercents)
 		}
 	}
 }
-
-void CVolumeChangeDialog::UpdateSelectionStatic()
+static CString GetSelectionText(long Start, long End, int Chan,
+								int nChannels, BOOL bLockChannels,
+								long nSamplesPerSec, int TimeFormat)
 {
 	CString s;
-	if (m_pWf->nChannels > 1)
+	if (nChannels > 1)
 	{
 		LPCTSTR sChans = _T("Stereo");
-		if (0 == m_Chan)
+		if (! bLockChannels)
 		{
-			sChans = _T("Left");
-		}
-		else if (1 == m_Chan)
-		{
-			sChans = _T("Right");
+			if (0 == Chan)
+			{
+				sChans = _T("Left");
+			}
+			else if (1 == Chan)
+			{
+				sChans = _T("Right");
+			}
 		}
 		s.Format("Selection : %s to %s (%s)\n"
 				"Channels: %s",
-				SampleToString(m_Start, m_pWf, m_TimeFormat),
-				SampleToString(m_End, m_pWf, m_TimeFormat),
-				SampleToString(m_End - m_Start, m_pWf, m_TimeFormat),
+				SampleToString(Start, nSamplesPerSec, TimeFormat),
+				SampleToString(End, nSamplesPerSec, TimeFormat),
+				SampleToString(End - Start, nSamplesPerSec, TimeFormat),
 				sChans);
 	}
 	else
 	{
 		s.Format("Selection : %s to %s (%s)",
-				SampleToString(m_Start, m_pWf, m_TimeFormat),
-				SampleToString(m_End, m_pWf, m_TimeFormat),
-				SampleToString(m_End - m_Start, m_pWf, m_TimeFormat));
+				SampleToString(Start, nSamplesPerSec, TimeFormat),
+				SampleToString(End, nSamplesPerSec, TimeFormat),
+				SampleToString(End - Start, nSamplesPerSec, TimeFormat));
 	}
-	m_SelectionStatic.SetWindowText(s);
+	return s;
+}
+
+void CVolumeChangeDialog::UpdateSelectionStatic()
+{
+	m_SelectionStatic.SetWindowText(GetSelectionText(m_Start, m_End, m_Chan,
+													m_pWf->nChannels, m_bLockChannels,
+													m_pWf->nSamplesPerSec, m_TimeFormat));
 }
 
 void CVolumeChangeDialog::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
+	// initialize slider control dirst, then edit control
 	//{{AFX_DATA_MAP(CVolumeChangeDialog)
 	DDX_Control(pDX, IDC_SLIDER_VOLUME_LEFT, m_SliderVolumeLeft);
 	DDX_Control(pDX, IDC_EDIT_VOLUME_LEFT, m_eVolumeLeft);
@@ -166,8 +178,8 @@ void CVolumeChangeDialog::DoDataExchange(CDataExchange* pDX)
 	if (m_pWf->nChannels > 1)
 	{
 		DDX_Check(pDX, IDC_CHECKLOCK_CHANNELS, m_bLockChannels);
-		DDX_Control(pDX, IDC_EDIT_VOLUME_RIGHT, m_eVolumeRight);
 		DDX_Control(pDX, IDC_SLIDER_VOLUME_RIGHT, m_SliderVolumeRight);
+		DDX_Control(pDX, IDC_EDIT_VOLUME_RIGHT, m_eVolumeRight);
 	}
 
 	UpdateVolumeData(pDX, m_DbPercent);
@@ -184,6 +196,9 @@ BEGIN_MESSAGE_MAP(CVolumeChangeDialog, CDialog)
 ON_BN_CLICKED(IDC_CHECKLOCK_CHANNELS, OnChecklockChannels)
 ON_BN_CLICKED(IDC_BUTTON_SELECTION, OnButtonSelection)
 ON_CBN_SELCHANGE(IDC_COMBODB_PERCENT, OnSelchangeCombodbPercent)
+	ON_WM_HSCROLL()
+	ON_EN_KILLFOCUS(IDC_EDIT_VOLUME_LEFT, OnKillfocusEditVolumeLeft)
+	ON_EN_KILLFOCUS(IDC_EDIT_VOLUME_RIGHT, OnKillfocusEditVolumeRight)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -193,18 +208,43 @@ END_MESSAGE_MAP()
 void CVolumeChangeDialog::OnChecklockChannels()
 {
 	m_bLockChannels = IsDlgButtonChecked(IDC_CHECKLOCK_CHANNELS);
-	m_eVolumeRight.EnableWindow( ! m_bLockChannels);
-	m_SliderVolumeRight.EnableWindow( ! m_bLockChannels);
-	GetDlgItem(IDC_STATIC_RIGHT_CHANNEL)->EnableWindow( ! m_bLockChannels);
+	UpdateEnables();
+	UpdateSelectionStatic();
+}
+
+void CVolumeChangeDialog::UpdateEnables()
+{
+	BOOL EnableLeft = m_bLockChannels || 1 != m_Chan;
+	BOOL EnableRight = ! m_bLockChannels && 0 != m_Chan;
+
+	m_eVolumeLeft.EnableWindow(EnableLeft);
+	m_SliderVolumeLeft.EnableWindow(EnableLeft);
+	GetDlgItem(IDC_STATIC_LEFT_CHANNEL)->EnableWindow(EnableLeft);
+
+	m_eVolumeRight.EnableWindow(EnableRight);
+	m_SliderVolumeRight.EnableWindow(EnableRight);
+	GetDlgItem(IDC_STATIC_RIGHT_CHANNEL)->EnableWindow(EnableRight);
 }
 
 void CVolumeChangeDialog::OnButtonSelection()
 {
 	CSelectionDialog dlg;
+	dlg.m_Start = m_Start;
+	dlg.m_End = m_End;
+	dlg.m_Length = m_End - m_Start;
+	dlg.m_FileLength = m_FileLength;
+	dlg.m_Chan = m_Chan;
+	dlg.m_pWf = m_pWf;
+	dlg.m_TimeFormat = m_TimeFormat;
+
 	if (IDOK != dlg.DoModal())
 	{
 		return;
 	}
+	m_Start = dlg.m_Start;
+	m_End = dlg.m_End;
+	m_Chan = dlg.m_Chan;
+	UpdateSelectionStatic();
 }
 
 void CVolumeChangeDialog::OnSelchangeCombodbPercent()
@@ -285,7 +325,9 @@ CSelectionDialog::CSelectionDialog(CWnd* pParent /*=NULL*/)
 	: CDialog(CSelectionDialog::IDD, pParent)
 {
 	//{{AFX_DATA_INIT(CSelectionDialog)
-	// NOTE: the ClassWizard will add member initialization here
+	m_Chan = -1;
+	m_TimeFormatIndex = 0;
+	m_SelectionNumber = 0;
 	//}}AFX_DATA_INIT
 }
 
@@ -294,15 +336,26 @@ void CSelectionDialog::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CSelectionDialog)
-	// NOTE: the ClassWizard will add DDX and DDV calls here
+	DDX_Control(pDX, IDC_EDIT_LENGTH, m_eLength);
+	DDX_Control(pDX, IDC_EDIT_START, m_eStart);
+	DDX_Control(pDX, IDC_EDIT_END, m_eEnd);
+	DDX_Radio(pDX, IDC_RADIO_CHANNEL, m_Chan);
+	DDX_CBIndex(pDX, IDC_COMBO_TIME_FORMAT, m_TimeFormatIndex);
+	DDX_CBIndex(pDX, IDC_COMBO_SELECTION, m_SelectionNumber);
 	//}}AFX_DATA_MAP
+	m_eStart.ExchangeData(pDX, m_Start);
+	m_eEnd.ExchangeData(pDX, m_End);
+	m_eLength.ExchangeData(pDX, m_Length);
 }
 
 
 BEGIN_MESSAGE_MAP(CSelectionDialog, CDialog)
 //{{AFX_MSG_MAP(CSelectionDialog)
-// NOTE: the ClassWizard will add message map macros here
-//}}AFX_MSG_MAP
+	ON_CBN_SELCHANGE(IDC_COMBO_TIME_FORMAT, OnSelchangeComboTimeFormat)
+	ON_EN_KILLFOCUS(IDC_EDIT_END, OnKillfocusEditEnd)
+	ON_EN_KILLFOCUS(IDC_EDIT_LENGTH, OnKillfocusEditLength)
+	ON_EN_KILLFOCUS(IDC_EDIT_START, OnKillfocusEditStart)
+	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -311,16 +364,337 @@ END_MESSAGE_MAP()
 BOOL CVolumeChangeDialog::OnInitDialog()
 {
 	CDialog::OnInitDialog();
+	m_SliderVolumeLeft.SetRange(-400, 400);
+	m_SliderVolumeLeft.SetTicFreq(100);
+	m_SliderVolumeLeft.SetLineSize(10);
+	m_SliderVolumeLeft.SetPageSize(50);
+	if (0 == m_DbPercent)
+	{
+		// decibel
+		m_SliderVolumeLeft.SetPos(int(m_dVolumeLeftDb * 10.));
+	}
+	else
+	{
+		m_SliderVolumeLeft.SetPos(int(200. * log10(m_dVolumeLeftPercent / 100.)));
+	}
 
 	if (m_pWf->nChannels > 1)
 	{
-		if (m_bLockChannels)
+		m_SliderVolumeRight.SetRange(-400, 400);
+		m_SliderVolumeRight.SetTicFreq(100);
+		m_SliderVolumeRight.SetLineSize(10);
+		m_SliderVolumeRight.SetPageSize(50);
+		if (0 == m_DbPercent)
 		{
-			m_eVolumeRight.EnableWindow(FALSE);
-			m_SliderVolumeRight.EnableWindow(FALSE);
-			GetDlgItem(IDC_STATIC_RIGHT_CHANNEL)->EnableWindow(FALSE);
+			// decibel
+			m_SliderVolumeRight.SetPos(int(m_dVolumeLeftDb * 10.));
 		}
+		else
+		{
+			m_SliderVolumeRight.SetPos(int(200. * log10(m_dVolumeLeftPercent / 100.)));
+		}
+		UpdateEnables();
 	}
 	return TRUE;  // return TRUE unless you set the focus to a control
 	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CVolumeChangeDialog::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	CDialog::OnHScroll(nSBCode, nPos, pScrollBar);
+	CSliderCtrl * pSlider = dynamic_cast<CSliderCtrl *>(pScrollBar);
+	if (NULL != pSlider)
+	{
+		int pos = pSlider->GetPos();
+		CString s;
+		if (0 == m_DbPercent)
+		{
+			s.Format("%.2f", pos / 10.);
+		}
+		else
+		{
+			s.Format("%.0f", 100. * pow(10., pos / 200.));
+		}
+		int id = pSlider->GetDlgCtrlID();
+		if (IDC_SLIDER_VOLUME_LEFT == id)
+		{
+			SetDlgItemText(IDC_EDIT_VOLUME_LEFT, s);
+			if (2 == m_pWf->nChannels
+				&& m_bLockChannels)
+			{
+				SetDlgItemText(IDC_EDIT_VOLUME_RIGHT, s);
+				m_SliderVolumeRight.SetPos(pos);
+			}
+		}
+		else if (IDC_SLIDER_VOLUME_RIGHT == id)
+		{
+			SetDlgItemText(IDC_EDIT_VOLUME_RIGHT, s);
+		}
+	}
+}
+
+void CVolumeChangeDialog::OnKillfocusEditVolumeLeft()
+{
+	double num;
+	CString s;
+	m_eVolumeLeft.GetWindowText(s);
+	int SliderPos = 0;
+	if (! s.IsEmpty()
+		&& m_eVolumeLeft.SimpleFloatParse(s, num))
+	{
+		if (0 == m_DbPercent)
+		{
+			// decibel
+			if (num < -40. || num > 40.)
+			{
+				return;
+			}
+			SliderPos = int(num * 10.);
+		}
+		else
+		{
+			// percent
+			if (num < 1.
+				|| num > 10000.)
+			{
+				return;
+			}
+			SliderPos = int(200. * log10(num / 100.));
+		}
+		m_SliderVolumeLeft.SetPos(SliderPos);
+	}
+}
+
+void CVolumeChangeDialog::OnKillfocusEditVolumeRight()
+{
+	double num;
+	CString s;
+	m_eVolumeRight.GetWindowText(s);
+	int SliderPos = 0;
+	if (! s.IsEmpty()
+		&& m_eVolumeRight.SimpleFloatParse(s, num))
+	{
+		if (0 == m_DbPercent)
+		{
+			// decibel
+			if (num < -40. || num > 40.)
+			{
+				return;
+			}
+			SliderPos = int(num * 10.);
+		}
+		else
+		{
+			// percent
+			if (num < 1.
+				|| num > 10000.)
+			{
+				return;
+			}
+			SliderPos = int(200. * log10(num / 100.));
+		}
+		m_SliderVolumeRight.SetPos(SliderPos);
+	}
+}
+
+BOOL CSelectionDialog::OnInitDialog()
+{
+	m_eStart.SetTimeFormat(m_TimeFormat);
+	m_eEnd.SetTimeFormat(m_TimeFormat);
+	m_eLength.SetTimeFormat(m_TimeFormat);
+	switch (m_TimeFormat & SampleToString_Mask)
+	{
+	case SampleToString_Sample:
+		m_TimeFormatIndex = 0;
+		break;
+	case SampleToString_HhMmSs:
+		m_TimeFormatIndex = 1;
+		break;
+	case SampleToString_Seconds: default:
+		m_TimeFormatIndex = 2;
+		break;
+	}
+	if (NULL != m_pWf)
+	{
+		m_eStart.SetSamplingRate(m_pWf->nSamplesPerSec);
+		m_eEnd.SetSamplingRate(m_pWf->nSamplesPerSec);
+		m_eLength.SetSamplingRate(m_pWf->nSamplesPerSec);
+	}
+	CDialog::OnInitDialog();
+
+	// TODO: Add extra initialization here
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CSelectionDialog::OnSelchangeComboTimeFormat()
+{
+	int sel = ((CComboBox *)GetDlgItem(IDC_COMBO_TIME_FORMAT))->GetCurSel();
+	int Format;
+	switch (sel)
+	{
+	case 0:
+		Format = SampleToString_Sample;
+		break;
+	case 1:
+		Format = SampleToString_HhMmSs | TimeToHhMmSs_NeedsMs | TimeToHhMmSs_NeedsHhMm;
+		break;
+	case 2:
+	default:
+		Format = SampleToString_Seconds | TimeToHhMmSs_NeedsMs;
+		break;
+	}
+	if (Format == m_TimeFormat)
+	{
+		return;
+	}
+	m_TimeFormat = Format;
+	m_Start = m_eStart.GetTimeSample();
+	m_eStart.SetTimeFormat(Format);
+	m_eStart.SetTimeSample(m_Start);
+	m_End = m_eEnd.GetTimeSample();
+	m_eEnd.SetTimeFormat(Format);
+	m_eEnd.SetTimeSample(m_End);
+	m_Length = m_End - m_Start;
+	m_eLength.SetTimeFormat(Format);
+	m_eLength.SetTimeSample(m_Length);
+}
+
+void CSelectionDialog::OnKillfocusEditEnd()
+{
+	m_End = m_eEnd.GetTimeSample();
+	m_eEnd.SetTimeSample(m_End);
+	m_Length = m_End - m_Start;
+	if (m_Length < 0) m_Length = 0;
+	m_eLength.SetTimeSample(m_Length);
+}
+
+void CSelectionDialog::OnKillfocusEditLength()
+{
+	m_Length = m_eLength.GetTimeSample();
+	m_eLength.SetTimeSample(m_Length);
+	m_End = m_Start + m_Length;
+	m_eEnd.SetTimeSample(m_End);
+}
+
+void CSelectionDialog::OnKillfocusEditStart()
+{
+	m_Start = m_eStart.GetTimeSample();
+	m_eStart.SetTimeSample(m_Start);
+	m_Length = m_End - m_Start;
+	if (m_Length < 0) m_Length = 0;
+	m_eLength.SetTimeSample(m_Length);
+}
+/////////////////////////////////////////////////////////////////////////////
+// CGotoDialog dialog
+
+
+CGotoDialog::CGotoDialog(CWnd* pParent /*=NULL*/)
+	: CDialog(CGotoDialog::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CGotoDialog)
+	m_TimeFormatIndex = -1;
+	//}}AFX_DATA_INIT
+}
+
+
+void CGotoDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CGotoDialog)
+	DDX_Control(pDX, IDC_EDIT_START, m_eStart);
+	DDX_CBIndex(pDX, IDC_COMBO_TIME_FORMAT, m_TimeFormatIndex);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CGotoDialog, CDialog)
+	//{{AFX_MSG_MAP(CGotoDialog)
+		// NOTE: the ClassWizard will add message map macros here
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CGotoDialog message handlers
+/////////////////////////////////////////////////////////////////////////////
+// CDcOffsetDialog dialog
+
+
+CDcOffsetDialog::CDcOffsetDialog(CWnd* pParent /*=NULL*/)
+	: CDialog(CDcOffsetDialog::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CDcOffsetDialog)
+	m_b5SecondsDC = FALSE;
+	m_bUndo = FALSE;
+	m_nDcOffset = 0;
+	m_DcSelectMode = -1;
+	//}}AFX_DATA_INIT
+}
+
+
+void CDcOffsetDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CDcOffsetDialog)
+	DDX_Check(pDX, IDC_CHECK_5SECONDS, m_b5SecondsDC);
+	DDX_Check(pDX, IDC_CHECK_UNDO, m_bUndo);
+	DDX_Text(pDX, IDC_EDIT_DC_OFFSET, m_nDcOffset);
+	DDX_Radio(pDX, IDC_RADIO_DC_SELECT, m_DcSelectMode);
+	//}}AFX_DATA_MAP
+	if (0 == m_DcSelectMode)
+	{
+		GetDlgItem(IDC_EDIT_DC_OFFSET)->EnableWindow(FALSE);
+		//GetDlgItem(IDC_EDIT_DC_OFFSET)->EnableWindow(FALSE);
+	}
+	GetDlgItem(IDC_STATIC_SELECTION)->SetWindowText(
+													GetSelectionText(m_Start, m_End, m_Chan,
+														m_pWf->nChannels, FALSE,
+														m_pWf->nSamplesPerSec, m_TimeFormat));
+}
+
+
+BEGIN_MESSAGE_MAP(CDcOffsetDialog, CDialog)
+	//{{AFX_MSG_MAP(CDcOffsetDialog)
+	ON_BN_CLICKED(IDC_BUTTON_SELECTION, OnButtonSelection)
+	ON_BN_CLICKED(IDC_RADIO_DC_SELECT, OnRadioDcSelect)
+	ON_BN_CLICKED(IDC_RADIO2, OnRadioAdjustSelectEdit)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CDcOffsetDialog message handlers
+
+void CDcOffsetDialog::OnButtonSelection()
+{
+	CSelectionDialog dlg;
+	dlg.m_Start = m_Start;
+	dlg.m_End = m_End;
+	dlg.m_Length = m_End - m_Start;
+	dlg.m_FileLength = m_FileLength;
+	dlg.m_Chan = m_Chan;
+	dlg.m_pWf = m_pWf;
+	dlg.m_TimeFormat = m_TimeFormat;
+
+	if (IDOK != dlg.DoModal())
+	{
+		return;
+	}
+	m_Start = dlg.m_Start;
+	m_End = dlg.m_End;
+	m_Chan = dlg.m_Chan;
+	GetDlgItem(IDC_STATIC_SELECTION)->SetWindowText(
+													GetSelectionText(m_Start, m_End, m_Chan,
+														m_pWf->nChannels, FALSE,
+														m_pWf->nSamplesPerSec, m_TimeFormat));
+}
+
+void CDcOffsetDialog::OnRadioDcSelect()
+{
+	GetDlgItem(IDC_EDIT_DC_OFFSET)->EnableWindow(FALSE);
+}
+
+void CDcOffsetDialog::OnRadioAdjustSelectEdit()
+{
+	GetDlgItem(IDC_EDIT_DC_OFFSET)->EnableWindow(TRUE);
 }
--- OperationDialogs.h
+++ OperationDialogs.h
@@ -7,6 +7,7 @@
 // OperationDialogs.h : header file
 //
 #include "NumEdit.h"
+#include "TimeEdit.h"
 
 /////////////////////////////////////////////////////////////////////////////
 // CCopyChannelsSelectDlg dialog
@@ -104,6 +105,7 @@ public:
 	double m_dVolumeRightPercent;
 	long m_Start;
 	long m_End;
+	long m_FileLength;
 	int m_Chan;
 	int m_TimeFormat;
 	const WAVEFORMATEX * m_pWf;
@@ -117,13 +119,17 @@ protected:
 // Implementation
 protected:
 	void UpdateVolumeData(CDataExchange* pDX, BOOL InPercents);
-	void CVolumeChangeDialog::UpdateSelectionStatic();
+	void UpdateSelectionStatic();
+	void UpdateEnables();
 	// Generated message map functions
 	//{{AFX_MSG(CVolumeChangeDialog)
 	afx_msg void OnChecklockChannels();
 	afx_msg void OnButtonSelection();
 	afx_msg void OnSelchangeCombodbPercent();
 	virtual BOOL OnInitDialog();
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnKillfocusEditVolumeLeft();
+	afx_msg void OnKillfocusEditVolumeRight();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
@@ -140,9 +146,19 @@ public:
 // Dialog Data
 	//{{AFX_DATA(CSelectionDialog)
 	enum { IDD = IDD_SELECTION_DIALOG };
-	// NOTE: the ClassWizard will add data members here
+	CTimeEdit	m_eLength;
+	CTimeEdit	m_eStart;
+	CTimeEdit	m_eEnd;
+	int		m_Chan;
+	int		m_TimeFormatIndex;
+	int		m_SelectionNumber;
 	//}}AFX_DATA
-
+	int		m_TimeFormat;
+	long m_Start;
+	long m_End;
+	long m_Length;
+	long m_FileLength;
+	const WAVEFORMATEX * m_pWf;
 
 // Overrides
 	// ClassWizard generated virtual function overrides
@@ -156,7 +172,87 @@ protected:
 
 	// Generated message map functions
 	//{{AFX_MSG(CSelectionDialog)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSelchangeComboTimeFormat();
+	afx_msg void OnKillfocusEditEnd();
+	afx_msg void OnKillfocusEditLength();
+	afx_msg void OnKillfocusEditStart();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+/////////////////////////////////////////////////////////////////////////////
+// CGotoDialog dialog
+
+class CGotoDialog : public CDialog
+{
+// Construction
+public:
+	CGotoDialog(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CGotoDialog)
+	enum { IDD = IDD_DIALOG_GOTO };
+	CTimeEdit	m_eStart;
+	int		m_TimeFormatIndex;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CGotoDialog)
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CGotoDialog)
 	// NOTE: the ClassWizard will add member functions here
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+/////////////////////////////////////////////////////////////////////////////
+// CDcOffsetDialog dialog
+
+class CDcOffsetDialog : public CDialog
+{
+// Construction
+public:
+	CDcOffsetDialog(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CDcOffsetDialog)
+	enum { IDD = IDD_DIALOG_DC_OFFSET };
+	BOOL	m_b5SecondsDC;
+	BOOL	m_bUndo;
+	int		m_nDcOffset;
+	int		m_DcSelectMode;
+	//}}AFX_DATA
+
+	long m_Start;
+	long m_End;
+	long m_FileLength;
+	int m_Chan;
+	int m_TimeFormat;
+	const WAVEFORMATEX * m_pWf;
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CDcOffsetDialog)
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	void UpdateSelectionStatic();
+
+	// Generated message map functions
+	//{{AFX_MSG(CDcOffsetDialog)
+	afx_msg void OnButtonSelection();
+	afx_msg void OnRadioDcSelect();
+	afx_msg void OnRadioAdjustSelectEdit();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
