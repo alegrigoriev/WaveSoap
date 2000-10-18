156a88a1620e2026dafc302621c83e1c480d64fc
Alexander Grigoriev <alegr@earthlink.net>
2000-10-17 19:58:35 -0700

    Resample code moved to Waveproc

--- Resample.cpp
+++ Resample.cpp
@@ -5,125 +5,14 @@
 #define M_PI        3.14159265358979323846
 #define M_PI_2      1.57079632679489661923
 #define M_PI_4      0.785398163397448309616
-void CResampleContext::FilterSoundResample()
-{
-	int Channels = m_DstFile.Channels();
-	int SrcSamples = m_SrcBufFilled - m_SrcBufUsed - Channels * m_SrcFilterLength;
-	const float * src = m_pSrcBuf + m_SrcBufUsed;
-
-	if (SrcSamples <= 0)
-	{
-		return;
-	}
-
-	for (int i = 0; i < DstBufSize; i+= Channels)
-	{
-		for (int ch = 0; ch < Channels; ch++)
-		{
-			unsigned __int32 Phase1 = m_Phase;
-			double OutSample = 0.;
-			for (int j = 0; ; j+= Channels)
-			{
-				int TableIndex = Phase1 >> ResampleIndexShift;
-				double PhaseFraction = int(Phase1 & ~(0xFFFFFFFF << ResampleIndexShift));
-				ASSERT(src + j + ch < m_pSrcBuf + SrcBufSize);
-				OutSample += src[j+ch] * (m_FilterBuf[TableIndex] +
-										PhaseFraction * (m_FilterDifBuf[TableIndex]
-											+ PhaseFraction * m_FilterDif2Buf[TableIndex]));
-				unsigned __int32 Phase2 = Phase1 + m_InputPeriod;
-				if (Phase2 < Phase1)
-				{
-					break;
-				}
-				Phase1 = Phase2;
-			}
-			m_pDstBuf[i+ch] = OutSample;
-		}
-		m_DstBufUsed += Channels;
-		m_Phase -= m_OutputPeriod;
-		while (m_Phase & 0x80000000)
-		{
-			src += Channels;
-			m_Phase += m_InputPeriod;
-			SrcSamples -= Channels;
-			m_SrcBufUsed += Channels;
-		}
-		if (SrcSamples < Channels)
-		{
-			return;
-		}
-	}
-}
-
 BOOL CResampleContext::InitResample(CWaveFile & SrcFile, CWaveFile &DstFile,
 									double FrequencyRatio, double FilterLength)
 {
 	m_SrcFile = SrcFile;
 	m_DstFile = DstFile;
-	// FrequencyRatio is out freq/ input freq. If >1, it is upsampling,
-	// if < 1 it is downsampling
-	// FilterLength is how many Sin periods are in the array
-	double PrevVal = 0.;
-	for (int i = 0; i < ResampleFilterSize; i++)
-	{
-		double arg = M_PI * FilterLength / ResampleFilterSize * (i + 1 - ResampleFilterSize / 2);
-		double Window = sin(M_PI * (i +1) / ResampleFilterSize);
-		Window *= Window;   // squared sin window
 
-		double arg1 = M_PI * FilterLength / ResampleFilterSize * (i + 0.5 - ResampleFilterSize / 2);
-		double Window1 = sin(M_PI * (i +0.5) / ResampleFilterSize);
-		Window1 *= Window1;   // squared sin window
-		double val;
-		if (arg != 0)
-		{
-			val = Window * sin(arg) / arg;
-		}
-		else
-		{
-			val = Window;   // window must be 1.
-		}
-		double val1 = Window1 * sin(arg1) / arg1;
-		// val *= FilterScale;
-		m_FilterBuf[i] = PrevVal;
-		//TRACE("Resample filter[%03d]=%f\n", i, m_FilterBuf[i]);
-		double dif1 = val1 - PrevVal;
-		double dif2 = val - val1;
-		double sqrdif = (dif2 - dif1) * 4. / 3.;
-		m_FilterDifBuf[i] = (val - PrevVal - sqrdif) / (1 << ResampleIndexShift);
-		m_FilterDif2Buf[i] = sqrdif / (1 << ResampleIndexShift) / (1 << ResampleIndexShift);
-		PrevVal = val;
-	}
-	if (FrequencyRatio >= 1.)
-	{
-		// upsampling.
-		//
-		double InputPeriod = 0x100000000i64 / (FilterLength * (1. + 1. / FilterLength));
-		m_InputPeriod = unsigned __int32(InputPeriod);
-		m_OutputPeriod = unsigned __int32(InputPeriod / FrequencyRatio);
-	}
-	else
-	{
-		// downsampling
-		double OutputPeriod = 0x100000000i64 / (FilterLength * (1. + 1. / FilterLength));
-		m_OutputPeriod = unsigned __int32(OutputPeriod);
-		m_InputPeriod = unsigned __int32(OutputPeriod * FrequencyRatio);
-	}
-	//TRACE("InputPeriod=%08x, OutputPeriod=%08x\n", m_InputPeriod, m_OutputPeriod);
+	m_Resample.InitResample(FrequencyRatio, FilterLength, SrcFile.Channels());
 
-	m_SrcFilterLength = 0x100000000i64 / m_InputPeriod;
-	m_Phase = 0x80000000u % m_InputPeriod;
-
-	m_pSrcBuf = new float[SrcBufSize];
-	m_pDstBuf = new float[DstBufSize];
-	if (NULL == m_pSrcBuf || NULL == m_pDstBuf)
-	{
-		return FALSE;
-	}
-	memset(m_pSrcBuf, 0, SrcBufSize * sizeof * m_pSrcBuf);
-	// prefill at 1/2 filter length
-	m_SrcBufFilled = (0x80000000u / m_InputPeriod) * SrcFile.Channels();
-	m_SrcBufUsed = 0;
-	m_DstBufUsed = 0;
 	WAVEFORMATEX * pWf = m_SrcFile.GetWaveFormat();
 	MMCKINFO * pCk = m_SrcFile.GetDataChunk();
 	m_SrcStart = pCk->dwDataOffset;
@@ -158,136 +47,110 @@ BOOL CResampleContext::OperationProc()
 	DWORD WasRead = 0;
 	DWORD WasLockedToWrite = 0;
 	void * pOriginalSrcBuf;
-	__int16 * pSrcBuf;
+	char * pSrcBuf;
 	void * pOriginalDstBuf = NULL;
-	__int16 * pDstBuf;
+	char * pDstBuf;
 	do
 	{
-		if ((m_SrcBufFilled - m_SrcBufUsed) / m_SrcFile.Channels()
-			<= m_SrcFilterLength * 2)
+		if (0 == LeftToRead)
 		{
-			for (int i = 0, j = m_SrcBufUsed; j < m_SrcBufFilled; i++, j++)
+			DWORD SizeToRead = m_SrcEnd - m_SrcCopyPos;
+			if (SizeToRead > 0x10000)
 			{
-				m_pSrcBuf[i] = m_pSrcBuf[j];
+				SizeToRead = 0x10000;
 			}
-			m_SrcBufUsed = 0;
-			m_SrcBufFilled = i;
-		}
-		while (m_SrcBufFilled < SrcBufSize)
-		{
-			if (0 == LeftToRead)
+			if (SizeToRead != 0)
 			{
-				DWORD SizeToRead = m_SrcEnd - m_SrcCopyPos;
-				if (0 == SizeToRead)
-				{
-					// fill the rest of the input buffer with zeros
-					for (int j = m_SrcBufFilled; j < SrcBufSize; j++)
-					{
-						m_pSrcBuf[j] = 0.;
-					}
-					m_SrcBufFilled = j;
-					break;
-				}
-				if (SizeToRead > 0x10000)
-				{
-					SizeToRead = 0x10000;
-				}
-				WasRead = m_SrcFile.m_File.GetDataBuffer( & pOriginalSrcBuf,
-														SizeToRead, m_SrcCopyPos, CDirectFile::GetBufferAndPrefetchNext);
+				WasRead = m_SrcFile.GetDataBuffer( & pOriginalSrcBuf,
+													SizeToRead, m_SrcCopyPos, CDirectFile::GetBufferAndPrefetchNext);
 				if (0 == WasRead)
 				{
 					if (0 != WasLockedToWrite)
 					{
-						m_DstFile.m_File.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-														CDirectFile::ReturnBufferDirty);
+						m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
+													CDirectFile::ReturnBufferDirty);
 					}
 					return FALSE;
 				}
-				pSrcBuf = (__int16 *) pOriginalSrcBuf;
+				pSrcBuf = (char *) pOriginalSrcBuf;
 				LeftToRead = WasRead;
 			}
-
-			int ToCopy = __min(SrcBufSize - m_SrcBufFilled, LeftToRead / sizeof *pSrcBuf);
-			for (int i = 0, j = m_SrcBufFilled; i < ToCopy; i++, j++)
+			else
 			{
-				m_pSrcBuf[j] = pSrcBuf[i];
-			}
-			m_SrcBufFilled = j;
-			pSrcBuf += i;
-			LeftToRead -= i * sizeof *pSrcBuf;
-			m_SrcCopyPos += i * sizeof *pSrcBuf;
-
-			ASSERT((char*)pSrcBuf + LeftToRead == WasRead + (char*)pOriginalSrcBuf);
-			if (0 == LeftToRead)
-			{
-				m_SrcFile.m_File.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-												CDirectFile::ReturnBufferDiscard);
+				pSrcBuf = NULL;
+				LeftToRead = 0;
 				WasRead = 0;
 			}
 		}
 
-		m_DstBufUsed = 0;
-		FilterSoundResample();
-		int DstBufSaved = 0;
-		while (DstBufSaved < m_DstBufUsed)
+		if (0 == LeftToWrite)
 		{
-			if (0 == LeftToWrite)
+			DWORD SizeToWrite = m_DstEnd - m_DstCopyPos;
+			if (0 == SizeToWrite)
 			{
-				DWORD SizeToWrite = m_DstEnd - m_DstCopyPos;
-				if (0 == SizeToWrite)
+				break;  // all data written
+			}
+			WasLockedToWrite = m_DstFile.GetDataBuffer( & pOriginalDstBuf,
+														SizeToWrite, m_DstCopyPos, m_GetBufferFlags);
+
+			if (0 == WasLockedToWrite)
+			{
+				if (0 != WasRead)
 				{
-					break;  // all data written
+					m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
+												CDirectFile::ReturnBufferDiscard);
 				}
-				WasLockedToWrite = m_DstFile.m_File.GetDataBuffer( & pOriginalDstBuf,
-										SizeToWrite, m_DstCopyPos, CDirectFile::GetBufferWriteOnly);
-
-				if (0 == WasLockedToWrite)
-				{
-					if (0 != WasRead)
-					{
-						m_SrcFile.m_File.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-														CDirectFile::ReturnBufferDiscard);
-					}
-					return FALSE;
-				}
-				pDstBuf = (__int16 *) pOriginalDstBuf;
-				LeftToWrite = WasLockedToWrite;
-
+				return FALSE;
 			}
+			pDstBuf = (char *) pOriginalDstBuf;
+			LeftToWrite = WasLockedToWrite;
 
-			int ToCopy = __min(m_DstBufUsed - DstBufSaved, LeftToWrite / sizeof *pDstBuf);
-			for (int i = 0, j = DstBufSaved; i < ToCopy; i++, j++)
-			{
-				pDstBuf[i] = m_pDstBuf[j];
-			}
-			DstBufSaved = j;
-			pDstBuf += i;
-			LeftToWrite -= i * sizeof *pDstBuf;
-			m_DstCopyPos += i * sizeof *pDstBuf;
-
-			ASSERT((char *)pDstBuf + LeftToWrite == WasLockedToWrite + (char*)pOriginalDstBuf);
-			if (0 == LeftToWrite)
-			{
-				m_DstFile.m_File.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-												CDirectFile::ReturnBufferDirty);
-				WasLockedToWrite = 0;
-			}
 		}
-		m_DstBufUsed = 0;
+
+		int SrcBufUsed = 0;
+
+		int DstBufUsed = m_Resample.ProcessSound(pSrcBuf, pDstBuf, LeftToRead,
+												LeftToWrite, & SrcBufUsed, m_SrcFile.Channels());
+
+		TRACE("ResampleContext: SrcPos=%d (0x%X), DstPos=%d (0x%X), src: %d bytes, dst: %d bytes\n",
+			m_SrcCopyPos, m_SrcCopyPos, m_DstCopyPos, m_DstCopyPos,
+			SrcBufUsed, DstBufUsed);
+		LeftToRead -= SrcBufUsed;
+		m_SrcCopyPos += SrcBufUsed;
+		pSrcBuf += SrcBufUsed;
+
+		if (0 == LeftToRead && 0 != WasRead)
+		{
+			m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
+										CDirectFile::ReturnBufferDiscard);
+			WasRead = 0;
+		}
+
+		LeftToWrite -= DstBufUsed;
+		m_DstCopyPos += DstBufUsed;
+		pDstBuf += DstBufUsed;
+
+		if (0 == LeftToWrite)
+		{
+			m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
+										CDirectFile::ReturnBufferDirty);
+			WasLockedToWrite = 0;
+		}
 
 	}
 	while (m_DstCopyPos < m_DstEnd
 			&& timeGetTime() - dwStartTime < 200
 			);
+
 	if (0 != WasRead)
 	{
-		m_SrcFile.m_File.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-										CDirectFile::ReturnBufferDiscard);
+		m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
+									CDirectFile::ReturnBufferDiscard);
 	}
 	if (0 != WasLockedToWrite)
 	{
-		m_DstFile.m_File.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-										CDirectFile::ReturnBufferDirty);
+		m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
+									CDirectFile::ReturnBufferDirty);
 	}
 
 	if (m_DstEnd > m_DstStart)
@@ -313,8 +176,11 @@ void CResampleContext::PostRetire(BOOL bChildContext)
 			m_pUndoContext->m_OldPeakDataGranularity =
 				pDocument->m_PeakDataGranularity;
 			m_pUndoContext->m_pOldPeaks = pDocument->m_pPeaks;
+
 			// detach peaks
 			pDocument->m_pPeaks = NULL;
+			pDocument->m_AllocatedWavePeakSize = 0;
+			pDocument->m_WavePeakSize = 0;
 
 			pDocument->AddUndoRedo(m_pUndoContext);
 			m_pUndoContext = NULL;
@@ -328,7 +194,7 @@ void CResampleContext::PostRetire(BOOL bChildContext)
 		long nSamples = pDocument->WaveFileSamples();
 		pDocument->SoundChanged(pDocument->WaveFileID(),
 								0, nSamples, nSamples, UpdateSoundDontRescanPeaks);
-		pDocument->BuildPeakInfo();
+		pDocument->BuildPeakInfo(FALSE);    // don't save it yet
 		//pDocument->UpdateAllViews(NULL);
 	}
 	else
--- Resample.h
+++ Resample.h
@@ -7,7 +7,7 @@
 // Resample.h : header file
 //
 #include "OperationContext.h"
-
+#include "waveproc.h"
 /////////////////////////////////////////////////////////////////////////////
 // Resample sound
 class CResampleContext : public CCopyContext
@@ -15,51 +15,20 @@ class CResampleContext : public CCopyContext
 public:
 	CResampleContext(CWaveSoapFrontDoc * pDoc,
 					LPCTSTR StatusString, LPCTSTR OperationName)
-		: CCopyContext(pDoc, StatusString, OperationName),
-		m_pDstBuf(NULL),
-		m_pSrcBuf(NULL)
+		: CCopyContext(pDoc, StatusString, OperationName)
 	{
+		m_GetBufferFlags = CDirectFile::GetBufferWriteOnly;
 	}
 	virtual ~CResampleContext()
 	{
-		if (m_pDstBuf)
-		{
-			delete[] m_pDstBuf;
-			m_pDstBuf = NULL;
-		}
-		if (m_pSrcBuf)
-		{
-			delete[] m_pSrcBuf;
-			m_pSrcBuf = NULL;
-		}
 	}
-	enum {ResampleTableBits = 10,
-		ResampleFilterSize = (1 << ResampleTableBits),
-		ResampleIndexShift = (32 - ResampleTableBits),
-		SrcBufSize = 0x4000,
-		DstBufSize = 0x4000 };
-	float *m_pSrcBuf;
-	float *m_pDstBuf;
 
-	int m_SrcBufUsed;
-	int m_DstBufUsed;
-
-	int m_SrcBufFilled;
-	int m_SrcFilterLength;
-
-	float m_FilterBuf[ResampleFilterSize];
-	float m_FilterDifBuf[ResampleFilterSize];
-	float m_FilterDif2Buf[ResampleFilterSize];
-
-	unsigned __int32 m_InputPeriod;
-	unsigned __int32 m_OutputPeriod;
-	unsigned __int32 m_Phase;
+	CResampleFilter m_Resample;
 
 	virtual BOOL OperationProc();
 	BOOL InitResample(CWaveFile & SrcFile, CWaveFile &DstFile,
 					double FrequencyRatio, double FilterLength);
 	virtual void PostRetire(BOOL bChildContext = FALSE);
-	void FilterSoundResample();
 };
 
 #endif //AFX_RESAMPLE_H__165978E0_39A0_11D4_9ADD_00C0F0583C4B__INCLUDED_
