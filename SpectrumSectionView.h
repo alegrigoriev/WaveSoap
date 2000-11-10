9a7c524c6a9108844ffb79fd0387061183613cfa
Alexander Grigoriev <alegr@earthlink.net>
2000-11-10 07:45:20 -0800

    Vertical Spectrum section view

---
+++ SpectrumSectionView.h
@@ -0,0 +1,53 @@
+#if !defined(AFX_SPECTRUMSECTIONVIEW_H__6686D1B7_327B_4095_B596_07E91D2CB8FB__INCLUDED_)
+#define AFX_SPECTRUMSECTIONVIEW_H__6686D1B7_327B_4095_B596_07E91D2CB8FB__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// SpectrumSectionView.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CSpectrumSectionView view
+
+class CSpectrumSectionView : public CView
+{
+protected:
+	CSpectrumSectionView();           // protected constructor used by dynamic creation
+	DECLARE_DYNCREATE(CSpectrumSectionView)
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CSpectrumSectionView)
+protected:
+	virtual void OnDraw(CDC* pDC);      // overridden to draw this view
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+	virtual ~CSpectrumSectionView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+	// Generated message map functions
+protected:
+	//{{AFX_MSG(CSpectrumSectionView)
+	// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SPECTRUMSECTIONVIEW_H__6686D1B7_327B_4095_B596_07E91D2CB8FB__INCLUDED_)
