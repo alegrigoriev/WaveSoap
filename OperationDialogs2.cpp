c1a271eb0056cfbef328920a547ba1a0c4d6275e
Alexander Grigoriev <alegr@earthlink.net>
2000-11-15 21:25:33 -0800

    InsertSilence dialog ready

--- OperationDialogs2.cpp
+++ OperationDialogs2.cpp
@@ -18,9 +18,13 @@ static char THIS_FILE[] = __FILE__;
 CInsertSilenceDialog::CInsertSilenceDialog(CWnd* pParent /*=NULL*/)
 	: CDialog(CInsertSilenceDialog::IDD, pParent)
 {
+	m_pWf = NULL;
 	//{{AFX_DATA_INIT(CInsertSilenceDialog)
-	// NOTE: the ClassWizard will add member initialization here
+	m_TimeFormatIndex = -1;
+	m_nChannel = -1;
 	//}}AFX_DATA_INIT
+	m_Length = 0;
+	m_Start = 0;
 }
 
 
@@ -28,16 +32,129 @@ void CInsertSilenceDialog::DoDataExchange(CDataExchange* pDX)
 {
 	CDialog::DoDataExchange(pDX);
 	//{{AFX_DATA_MAP(CInsertSilenceDialog)
-	// NOTE: the ClassWizard will add DDX and DDV calls here
+	DDX_Control(pDX, IDC_EDIT_START, m_eStart);
+	DDX_Control(pDX, IDC_EDIT_LENGTH, m_eLength);
+	DDX_CBIndex(pDX, IDC_COMBO_TIME_FORMAT, m_TimeFormatIndex);
+	DDX_Radio(pDX, IDC_RADIO_CHANNEL, m_nChannel);
 	//}}AFX_DATA_MAP
+	m_eLength.ExchangeData(pDX, m_Length);
+	m_eStart.ExchangeData(pDX, m_Start);
 }
 
 
 BEGIN_MESSAGE_MAP(CInsertSilenceDialog, CDialog)
 	//{{AFX_MSG_MAP(CInsertSilenceDialog)
-		// NOTE: the ClassWizard will add message map macros here
+	ON_CBN_SELCHANGE(IDC_COMBO_TIME_FORMAT, OnSelchangeComboTimeFormat)
+	ON_EN_KILLFOCUS(IDC_EDIT_LENGTH, OnKillfocusEditLength)
+	ON_EN_KILLFOCUS(IDC_EDIT_START, OnKillfocusEditStart)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
 // CInsertSilenceDialog message handlers
+
+void CInsertSilenceDialog::OnSelchangeComboTimeFormat()
+{
+	int sel = ((CComboBox *)GetDlgItem(IDC_COMBO_TIME_FORMAT))->GetCurSel();
+	int Format;
+	switch (sel)
+	{
+	case 0:
+		Format = SampleToString_Sample;
+		break;
+	case 1:
+		Format = SampleToString_HhMmSs | TimeToHhMmSs_NeedsMs | TimeToHhMmSs_NeedsHhMm;
+		break;
+	case 2:
+	default:
+		Format = SampleToString_Seconds | TimeToHhMmSs_NeedsMs;
+		break;
+	}
+	if (Format == m_TimeFormat)
+	{
+		return;
+	}
+	m_TimeFormat = Format;
+	m_Length = m_eLength.GetTimeSample();
+	m_eLength.SetTimeFormat(Format);
+	m_eLength.SetTimeSample(m_Length);
+	m_Start = m_eStart.GetTimeSample();
+	m_eStart.SetTimeFormat(Format);
+	m_eStart.SetTimeSample(m_Start);
+}
+
+
+BOOL CInsertSilenceDialog::OnInitDialog()
+{
+	m_eLength.SetTimeFormat(m_TimeFormat);
+	m_eStart.SetTimeFormat(m_TimeFormat);
+	switch (m_TimeFormat & SampleToString_Mask)
+	{
+	case SampleToString_Sample:
+		m_TimeFormatIndex = 0;
+		break;
+	case SampleToString_HhMmSs:
+		m_TimeFormatIndex = 1;
+		break;
+	case SampleToString_Seconds: default:
+		m_TimeFormatIndex = 2;
+		break;
+	}
+	if (NULL != m_pWf)
+	{
+		m_eLength.SetSamplingRate(m_pWf->nSamplesPerSec);
+		m_eStart.SetSamplingRate(m_pWf->nSamplesPerSec);
+	}
+	CDialog::OnInitDialog();
+
+	return TRUE;  // return TRUE unless you set the focus to a control
+	// EXCEPTION: OCX Property Pages should return FALSE
+}
+
+void CInsertSilenceDialog::OnKillfocusEditLength()
+{
+	m_Length = m_eLength.GetTimeSample();
+	m_eLength.SetTimeSample(m_Length);
+}
+
+void CInsertSilenceDialog::OnKillfocusEditStart()
+{
+	m_Start = m_eStart.GetTimeSample();
+	m_eStart.SetTimeSample(m_Start);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CSilenceOptionDialog dialog
+
+
+CSilenceOptionDialog::CSilenceOptionDialog(CWnd* pParent /*=NULL*/)
+	: CDialog(CSilenceOptionDialog::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CSilenceOptionDialog)
+	// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CSilenceOptionDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CSilenceOptionDialog)
+	// NOTE: the ClassWizard will add DDX and DDV calls here
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CSilenceOptionDialog, CDialog)
+	//{{AFX_MSG_MAP(CSilenceOptionDialog)
+	ON_BN_CLICKED(IDC_BUTTON_SILENCE, OnButtonSilence)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CSilenceOptionDialog message handlers
+
+void CSilenceOptionDialog::OnButtonSilence()
+{
+	EndDialog(IDC_BUTTON_SILENCE);
+}
