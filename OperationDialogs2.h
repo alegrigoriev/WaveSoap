eb8ceae26cca1fa9bb2155e96aab04a1feec2551
Alexander Grigoriev <alegr@earthlink.net>
2000-11-15 21:25:33 -0800

    InsertSilence dialog ready

--- OperationDialogs2.h
+++ OperationDialogs2.h
@@ -6,6 +6,7 @@
 #endif // _MSC_VER > 1000
 // OperationDialogs2.h : header file
 //
+#include "TimeEdit.h"
 
 /////////////////////////////////////////////////////////////////////////////
 // CInsertSilenceDialog dialog
@@ -19,9 +20,16 @@ public:
 // Dialog Data
 	//{{AFX_DATA(CInsertSilenceDialog)
 	enum { IDD = IDD_DIALOG_INSERT_SILENCE };
-	// NOTE: the ClassWizard will add data members here
+	CTimeEdit	m_eLength;
+	CTimeEdit	m_eStart;
+	int		m_nChannel;
+	int		m_TimeFormatIndex;
 	//}}AFX_DATA
 
+	int		m_TimeFormat;
+	long    m_Length;
+	long    m_Start;
+	WAVEFORMATEX * m_pWf;
 
 // Overrides
 	// ClassWizard generated virtual function overrides
@@ -35,11 +43,46 @@ protected:
 
 	// Generated message map functions
 	//{{AFX_MSG(CInsertSilenceDialog)
-	// NOTE: the ClassWizard will add member functions here
+	afx_msg void OnSelchangeComboTimeFormat();
+	virtual BOOL OnInitDialog();
+	afx_msg void OnKillfocusEditStart();
+	afx_msg void OnKillfocusEditLength();
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
 
+/////////////////////////////////////////////////////////////////////////////
+// CSilenceOptionDialog dialog
+
+class CSilenceOptionDialog : public CDialog
+{
+// Construction
+public:
+	CSilenceOptionDialog(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CSilenceOptionDialog)
+	enum { IDD = IDD_DIALOG_MUTE_OR_SILENCE };
+	// NOTE: the ClassWizard will add data members here
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CSilenceOptionDialog)
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CSilenceOptionDialog)
+	afx_msg void OnButtonSilence();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
 
