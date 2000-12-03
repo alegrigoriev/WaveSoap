8f81b1f8e7cfeabf4ca671c0744e7dfbd77e9a80
Alexander Grigoriev <alegr@earthlink.net>
2000-12-02 19:51:25 -0800

    CD drive reading support, through IOCTL or WINASPI

---
+++ CdDrive.cpp
@@ -0,0 +1,27 @@
+// CdDrive.cpp: implementation of the CCdDrive class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "WaveSoapFront.h"
+#include "CdDrive.h"
+
+#ifdef _DEBUG
+#undef THIS_FILE
+static char THIS_FILE[]=__FILE__;
+#define new DEBUG_NEW
+#endif
+
+//////////////////////////////////////////////////////////////////////
+// Construction/Destruction
+//////////////////////////////////////////////////////////////////////
+
+CCdDrive::CCdDrive()
+{
+
+}
+
+CCdDrive::~CCdDrive()
+{
+
+}
---
+++ CdDrive.h
@@ -0,0 +1,20 @@
+// CdDrive.h: interface for the CCdDrive class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_CDDRIVE_H__444EC2DA_90D7_4205_BD0C_E0A478C802CD__INCLUDED_)
+#define AFX_CDDRIVE_H__444EC2DA_90D7_4205_BD0C_E0A478C802CD__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class CCdDrive
+{
+public:
+	CCdDrive();
+	virtual ~CCdDrive();
+
+};
+
+#endif // !defined(AFX_CDDRIVE_H__444EC2DA_90D7_4205_BD0C_E0A478C802CD__INCLUDED_)
