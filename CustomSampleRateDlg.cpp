2f738bbdc827915204a3dd12a1a4a4b3568e23c3
Alexander Grigoriev <alegr@earthlink.net>
2001-07-05 20:57:22 -0700

    Added CustomSampleRateDlg.cpp, CustomSampleRateDlg.h

---
+++ CustomSampleRateDlg.cpp
@@ -0,0 +1,43 @@
+// CustomSampleRateDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "WaveSoapFront.h"
+#include "CustomSampleRateDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CCustomSampleRateDlg dialog
+
+
+CCustomSampleRateDlg::CCustomSampleRateDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CCustomSampleRateDlg::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CCustomSampleRateDlg)
+	// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CCustomSampleRateDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CCustomSampleRateDlg)
+	// NOTE: the ClassWizard will add DDX and DDV calls here
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CCustomSampleRateDlg, CDialog)
+	//{{AFX_MSG_MAP(CCustomSampleRateDlg)
+		// NOTE: the ClassWizard will add message map macros here
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CCustomSampleRateDlg message handlers
