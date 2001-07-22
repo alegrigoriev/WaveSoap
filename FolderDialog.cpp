2262cc4f8525e16917636e0d6a30dd965e14cc4e
Alexander Grigoriev <alegr@earthlink.net>
2001-07-22 08:38:39 -0700

    Added FolderDialog.cpp, FolderDialog.h

---
+++ FolderDialog.cpp
@@ -0,0 +1,196 @@
+// FolderDialog.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "FolderDialog.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CFolderDialog
+
+IMPLEMENT_DYNAMIC(CFolderDialog, CDialog)
+
+CFolderDialog::CFolderDialog(LPCTSTR lpszTitle, LPCTSTR lpszStartingDirectory,
+							bool EnableCreateFolder,
+							DWORD dwFlags, CWnd* pParentWnd)
+	: CCommonDialog(pParentWnd),
+	szStartupDir(lpszStartingDirectory),
+	m_bEnableCreateDir(EnableCreateFolder)
+{
+	memset( & m_bi, 0, sizeof m_bi);
+	m_bi.ulFlags = dwFlags;
+	if (EnableCreateFolder)
+	{
+		m_bi.ulFlags |= BIF_EDITBOX;
+	}
+
+	m_bi.pszDisplayName = szBuffer;
+	szBuffer[0] = 0;
+	m_bi.lpszTitle = lpszTitle;
+	m_bi.lpfn = BrowseCallbackProc;
+	m_bi.lParam = (LPARAM) this;
+}
+
+BEGIN_MESSAGE_MAP(CFolderDialog, CCommonDialog)
+	//{{AFX_MSG_MAP(CFolderDialog)
+		// NOTE - the ClassWizard will add and remove mapping macros here.
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+void AFXAPI AfxHookWindowCreate(CWnd* pWnd);
+BOOL AFXAPI AfxUnhookWindowCreate();
+int CFolderDialog::DoModal()
+{
+	ASSERT_VALID(this);
+	ASSERT(m_bi.lpfn != NULL); // can still be a user hook
+
+	// WINBUG: This is a special case for the file open/save dialog,
+	//  which sometimes pumps while it is coming up but before it has
+	//  disabled the main window.
+	HWND hWndFocus = ::GetFocus();
+	BOOL bEnableParent = FALSE;
+	m_bi.hwndOwner = PreModal();
+	AfxUnhookWindowCreate();
+	if (m_bi.hwndOwner != NULL && ::IsWindowEnabled(m_bi.hwndOwner))
+	{
+		bEnableParent = TRUE;
+		::EnableWindow(m_bi.hwndOwner, FALSE);
+	}
+
+	OleInitialize(NULL);
+//#if 0
+	_AFX_THREAD_STATE* pThreadState = AfxGetThreadState();
+	ASSERT(pThreadState->m_pAlternateWndInit == NULL);
+
+	if (m_bi.ulFlags & BIF_NEWDIALOGSTYLE)
+		pThreadState->m_pAlternateWndInit = this;
+	else
+		AfxHookWindowCreate(this);
+//#endif
+
+	ASSERT(m_bi.lParam == (LPARAM) this);
+
+	LPITEMIDLIST lpResult = ::SHBrowseForFolder(&m_bi);
+	if (lpResult)
+	{
+		szDisplayName = m_bi.pszDisplayName;
+		IMalloc * pMalloc = NULL;
+		TCHAR Buffer[MAX_PATH + 1];
+		if (SHGetPathFromIDList(lpResult, Buffer))
+		{
+			szPath = Buffer;
+		}
+		if (SUCCEEDED(SHGetMalloc( & pMalloc)))
+		{
+			pMalloc->Free(lpResult);
+			pMalloc->Release();
+			pMalloc = NULL;
+		}
+	}
+	OleUninitialize();
+
+//#if 0
+	//if (lpResult)
+	//	ASSERT(pThreadState->m_pAlternateWndInit == NULL);
+	pThreadState->m_pAlternateWndInit = NULL;
+//#endif
+
+	// WINBUG: Second part of special case for file open/save dialog.
+	if (bEnableParent)
+		::EnableWindow(m_bi.hwndOwner, TRUE);
+	if (::IsWindow(hWndFocus))
+		::SetFocus(hWndFocus);
+
+	PostModal();
+	return (lpResult != 0) ? IDOK : IDCANCEL;
+}
+
+int CALLBACK CFolderDialog::BrowseCallbackProc(HWND hwnd,
+												UINT uMsg, LPARAM lParam, LPARAM lpData)
+{
+	CFolderDialog * pDlg = (CFolderDialog *) lpData;
+	switch (uMsg)
+	{
+	case BFFM_INITIALIZED:
+		TRACE("CFolderDialog::BrowseCallbackProc BFFM_INITIALIZED hwnd=%X\n", hwnd);
+		if(pDlg->m_hWnd == NULL)
+		{
+			pDlg->Attach(hwnd);
+		}
+		ASSERT(pDlg->m_hWnd == hwnd);
+		return pDlg->OnInitDone();
+		break;
+	case BFFM_SELCHANGED:
+		TRACE("CFolderDialog::BrowseCallbackProc BFFM_SELCHANGED hwnd=%X\n", hwnd);
+		if(pDlg->m_hWnd == NULL)
+		{
+			pDlg->Attach(hwnd);
+		}
+		ASSERT(pDlg->m_hWnd == hwnd);
+		return pDlg->OnFolderChange((LPITEMIDLIST) lParam);
+		break;
+
+	case BFFM_VALIDATEFAILED:
+		TRACE("CFolderDialog::BrowseCallbackProc BFFM_VALIDATEFAILED hwnd=%X\n", hwnd);
+		ASSERT(pDlg->m_hWnd == hwnd);
+		return pDlg->OnValidateFailed((LPCTSTR)lParam);
+		break;
+
+	default:
+		return 0;
+		break;
+	}
+}
+
+int CFolderDialog::OnInitDone()
+{
+	if ( ! m_bEnableCreateDir)
+	{
+		// disable and hide CreateDir button
+		CWnd * button = GetDlgItem(0x3746);
+		if (button)
+		{
+			button->EnableWindow(FALSE);
+			button->ShowWindow(SW_HIDE);
+		}
+	}
+	if (! szStartupDir.IsEmpty())
+	{
+		::SendMessage(m_hWnd, BFFM_SETSELECTION, TRUE,
+					(LPARAM)(LPCTSTR)szStartupDir);
+	}
+
+	return 0;
+}
+
+int CFolderDialog::OnFolderChange(LPITEMIDLIST lpItem)
+{
+	return 0;
+}
+
+int CFolderDialog::OnValidateFailed(LPCTSTR /*ErrorName*/)
+{
+	return 0;
+}
+
+CString CFolderDialog::GetFolderDisplayName() const
+{
+	return szDisplayName;
+}
+
+CString CFolderDialog::GetFolderPath() const
+{
+	return szPath;
+}
+
+#ifdef _DEBUG
+void CFolderDialog::Dump(CDumpContext& dc) const
+{
+}
+#endif
+
---
+++ FolderDialog.h
@@ -0,0 +1,64 @@
+#if !defined(AFX_FOLDERDIALOG_H__D77B1122_F648_11D1_B1C9_00A0244CBB12__INCLUDED_)
+#define AFX_FOLDERDIALOG_H__D77B1122_F648_11D1_B1C9_00A0244CBB12__INCLUDED_
+
+#if _MSC_VER >= 1000
+#pragma once
+#endif // _MSC_VER >= 1000
+// FolderDialog.h : header file
+//
+//#ifndef _SHLOBJ_H_
+#include <shlobj.h>
+//#endif
+/////////////////////////////////////////////////////////////////////////////
+// CFolderDialog dialog
+
+class CFolderDialog : public CCommonDialog
+{
+	DECLARE_DYNAMIC(CFolderDialog)
+
+public:
+	CFolderDialog(LPCTSTR lpszTitle = NULL,
+				LPCTSTR lpszStartingDirectory = NULL,
+				bool EnableCreateFolder = false,
+				DWORD dwFlags = BIF_RETURNONLYFSDIRS | BIF_NEWDIALOGSTYLE,
+				CWnd* pParentWnd = NULL);
+
+	BROWSEINFO m_bi;
+	TCHAR szBuffer[MAX_PATH + 1];
+	bool m_bEnableCreateDir;
+	CString szStartupDir;
+	CString szDisplayName;
+	CString szPath;
+// Operations
+	virtual int DoModal();
+
+	CString GetFolderDisplayName() const;
+	CString GetFolderPath() const;
+
+// Overridable callbacks
+	virtual int OnInitDone();
+	virtual int OnFolderChange(LPITEMIDLIST lpItem);
+	virtual int OnValidateFailed(LPCTSTR ErrorName);
+
+protected:
+	static int CALLBACK BrowseCallbackProc(HWND hwnd,
+											UINT uMsg, LPARAM lParam, LPARAM lpData);
+
+
+// Implementation
+#ifdef _DEBUG
+public:
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+	//{{AFX_MSG(CFolderDialog)
+	// NOTE - the ClassWizard will add and remove member functions here.
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Developer Studio will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_FOLDERDIALOG_H__D77B1122_F648_11D1_B1C9_00A0244CBB12__INCLUDED_)
