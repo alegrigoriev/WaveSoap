c3d1b1c770f26494d1ebec20299f4782e8e13570
Alexander Grigoriev <alegr@earthlink.net>
2001-10-08 21:21:59 -0700

    Click in outline view correctly goes to the largest peak

--- WaveOutlineView.h
+++ WaveOutlineView.h
@@ -57,6 +57,7 @@ protected:
 	afx_msg void OnLButtonUp(UINT nFlags, CPoint point);
 	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
 	afx_msg void OnCaptureChanged(CWnd *pWnd);
+	afx_msg BOOL OnSetCursor(CWnd* pWnd, UINT nHitTest, UINT message);
 	//}}AFX_MSG
 	DECLARE_MESSAGE_MAP()
 };
