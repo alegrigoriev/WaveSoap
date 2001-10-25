fb059b185528e673f0bd3873805568a76b19ee62
Alexander Grigoriev <alegr@earthlink.net>
2001-10-25 08:23:06 -0700

    Added ResizableDialog.cpp, ResizableDialog.h

---
+++ ResizableDialog.cpp
@@ -0,0 +1,43 @@
+// ResizableDialog.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "wavesoapfront.h"
+#include "ResizableDialog.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CResizableDialog dialog
+
+
+CResizableDialog::CResizableDialog(CWnd* pParent /*=NULL*/)
+	: CDialog(CResizableDialog::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CResizableDialog)
+	// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CResizableDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CResizableDialog)
+	// NOTE: the ClassWizard will add DDX and DDV calls here
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CResizableDialog, CDialog)
+	//{{AFX_MSG_MAP(CResizableDialog)
+		// NOTE: the ClassWizard will add message map macros here
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CResizableDialog message handlers
---
+++ ResizableDialog.h
@@ -0,0 +1,46 @@
+#if !defined(AFX_RESIZABLEDIALOG_H__1E590ECD_BEE6_4C1C_9E9C_A8F58A8FA54A__INCLUDED_)
+#define AFX_RESIZABLEDIALOG_H__1E590ECD_BEE6_4C1C_9E9C_A8F58A8FA54A__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// ResizableDialog.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CResizableDialog dialog
+
+class CResizableDialog : public CDialog
+{
+// Construction
+public:
+	CResizableDialog(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CResizableDialog)
+	enum { IDD = IDD_ABOUTBOX };
+	// NOTE: the ClassWizard will add data members here
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CResizableDialog)
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CResizableDialog)
+	// NOTE: the ClassWizard will add member functions here
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_RESIZABLEDIALOG_H__1E590ECD_BEE6_4C1C_9E9C_A8F58A8FA54A__INCLUDED_)
