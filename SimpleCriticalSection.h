90a171bc6df7cd727337f7da9abdb33bd69be7a7
Alexander Grigoriev <alegr@earthlink.net>
2003-04-12 17:46:21 -0700

    Changed SimpleCriticalSection.h
    Added copyright header
    Removed "throw()" qualifier

--- SimpleCriticalSection.h
+++ SimpleCriticalSection.h
@@ -1,3 +1,4 @@
+// Copyright Alexander Grigoriev, 1997-2002, All Rights Reserved
 #if !defined(AFX_SIMPLECRITICALSECTION_H__B7AA7401_4036_11D4_9ADD_00C0F0583C4B__INCLUDED_)
 #define AFX_SIMPLECRITICALSECTION_H__B7AA7401_4036_11D4_9ADD_00C0F0583C4B__INCLUDED_
 
@@ -9,19 +10,19 @@ class CSimpleCriticalSection
 {
 	CRITICAL_SECTION m_cs;
 public:
-	CSimpleCriticalSection() throw()
+	CSimpleCriticalSection()
 	{
 		InitializeCriticalSection( & m_cs);
 	}
-	~CSimpleCriticalSection() throw()
+	~CSimpleCriticalSection()
 	{
 		DeleteCriticalSection( & m_cs);
 	}
-	void Lock() volatile throw()
+	void Lock() volatile
 	{
 		EnterCriticalSection(const_cast<CRITICAL_SECTION *>( & m_cs));
 	}
-	void Unlock() volatile throw()
+	void Unlock() volatile
 	{
 		LeaveCriticalSection(const_cast<CRITICAL_SECTION *>( & m_cs));
 	}
