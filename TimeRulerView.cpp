7451aa0b4676a4203641b85ff5565aa5cf8f4602
Alexander Grigoriev <alegr@earthlink.net>
2004-08-14 19:03:25 -0700

    Changed TimeRulerView.cpp
    OnDraw goes through unified function,
    to prepare for region/marker drawing
    Fix: Black tick is now drawn in the same position as caret

--- TimeRulerView.cpp
+++ TimeRulerView.cpp
@@ -15,6 +15,18 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
+static int fround(double d)
+{
+	if (d >= 0.)
+	{
+		return int(d + 0.5);
+	}
+	else
+	{
+		return int(d - 0.5);
+	}
+}
+
 /////////////////////////////////////////////////////////////////////////////
 // CTimeRulerView
 
@@ -46,29 +58,11 @@ END_MESSAGE_MAP()
 
 void CTimeRulerView::OnDraw(CDC* pDC)
 {
-	switch (m_CurrentDisplayMode)
-	{
-	case ShowSamples:
-		DrawRulerSamples(pDC);
-		break;
-	case ShowHhMmSs:
-		DrawRulerHhMmSs(pDC);
-		break;
-	case ShowSeconds:
-		DrawRulerSeconds(pDC);
-	default:
-		break;
-	}
-}
-
-void CTimeRulerView::DrawRulerSamples(CDC* pDC)
-{
 	CWaveSoapFrontDoc* pDoc = GetDocument();
 	if (! pDoc->m_WavFile.IsOpen())
 	{
 		return;
 	}
-
 	// background is erased by gray brush.
 	// draw horizontal line with ticks and numbers
 	CGdiObjectSave OldFont(pDC, pDC->SelectStockObject(ANSI_VAR_FONT));
@@ -76,45 +70,6 @@ void CTimeRulerView::DrawRulerSamples(CDC* pDC)
 	CPen DarkGrayPen(PS_SOLID, 0, 0x808080);
 	CRect cr;
 	GetClientRect( & cr);
-
-	// calculate position string length
-	CString s;
-
-	int nLength = pDC->GetTextExtent(_T("0,000,000,000"), 13).cx;
-
-	NUMBER_OF_SAMPLES nSamples = NUMBER_OF_SAMPLES(1.5 * nLength / GetXScaleDev());
-	// calculate how much samples can be between the numbers
-	if (nSamples > INT_MAX / 10)
-	{
-		nSamples = INT_MAX / 10;
-	}
-
-	int dist = 1;
-	unsigned nTickDist = 1;
-	// find the closest bigger 1,2,5 * 10^x
-	for (unsigned k = 1; k <= INT_MAX / 10; k *= 10)
-	{
-		dist = k;
-		nTickDist = k;
-		if (dist >= nSamples)
-		{
-			if (dist > 10)
-			{
-				nTickDist = dist / 10;
-			}
-			break;
-		}
-		dist = k * 2;
-		if (dist >= nSamples)
-		{
-			break;
-		}
-		dist = k * 5;
-		if (dist >= nSamples)
-		{
-			break;
-		}
-	}
 
 	pDC->SetTextAlign(TA_BOTTOM | TA_LEFT);
 	pDC->SetTextColor(0x000000);   // black
@@ -133,136 +88,183 @@ void CTimeRulerView::DrawRulerSamples(CDC* pDC)
 	pDC->MoveTo(cr.left, cr.bottom - 6);
 	pDC->LineTo(cr.right, cr.bottom - 6);
 
-	NUMBER_OF_SAMPLES nFirstSample = NUMBER_OF_SAMPLES(WindowToWorldX(cr.left - nLength));
-
-	if (nFirstSample < 0)
-	{
-		nFirstSample = 0;
-	}
-	// round it
-	nFirstSample -= nFirstSample % dist;
-	for(;; nFirstSample += nTickDist)
-	{
-		if (nFirstSample > pDoc->WaveFileSamples())
-		{
-			break;
-		}
-		int x = WorldToWindowXrnd(nFirstSample);
-
-		if (x > cr.right)
-		{
-			break;
-		}
-		pDC->SelectStockObject(BLACK_PEN);
-		pDC->MoveTo(x - 1, cr.bottom - 6);
-
-		if (0 == nFirstSample % dist)
-		{
-			// draw bigger tick (6 pixels high) and the number
-			pDC->LineTo(x - 1, cr.bottom - 12);
-			CString s = LtoaCS(nFirstSample);
-
-			pDC->TextOut(x + 2, cr.bottom - 9, s);
-
-			pDC->SelectStockObject(WHITE_PEN);
-			pDC->MoveTo(x, cr.bottom - 6);
-			pDC->LineTo(x, cr.bottom - 12);
-		}
-		else
-		{
-			// draw small tick (2 pixels high)
-			pDC->LineTo(x - 1, cr.bottom - 8);
-
-			pDC->SelectStockObject(WHITE_PEN);
-			pDC->MoveTo(x, cr.bottom - 6);
-			pDC->LineTo(x, cr.bottom - 8);
-		}
-	}
-
-}
-
-void CTimeRulerView::DrawRulerHhMmSs(CDC* pDC)
-{
-	CWaveSoapFrontDoc* pDoc = GetDocument();
-	if (! pDoc->m_WavFile.IsOpen())
-	{
-		return;
-	}
-
-	// background is erased by gray brush.
-	// draw horizontal line with ticks and numbers
-	CGdiObjectSave OldFont(pDC, pDC->SelectStockObject(ANSI_VAR_FONT));
-	CPen DarkGrayPen(PS_SOLID, 0, 0x808080);
-	CRect cr;
-	GetClientRect( & cr);
-
-	// calculate position string length
+	TCHAR const DecimalPoint = GetApp()->m_DecimalPoint;
+	float const SampleRate = float(pDoc->WaveSampleRate());
 	int nTickCount;
-	float SampleRate = float(pDoc->WaveSampleRate());
+	double DistTime;
+	double nFirstTime;
+	int nLength;
 
-	int nLength = pDC->GetTextExtent(_T("00:00:00.0000"), 13).cx;
+	CString s;
 
-	double DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
-	// select distance between ticks
-	double multiplier = 1.;
-	double divisor = 1.;
-	if (DistTime >= 1.)
+	switch (m_CurrentDisplayMode)
 	{
-		if (DistTime > 3600.)
+	case ShowSamples:
+	{
+		// calculate position string length
+
+		nLength = pDC->GetTextExtent(_T("0,000,000,000"), 13).cx;
+
+		NUMBER_OF_SAMPLES nSamples = NUMBER_OF_SAMPLES(1.5 * nLength / GetXScaleDev());
+		// calculate how much samples can be between the numbers
+		if (nSamples > INT_MAX / 10)
 		{
-			multiplier = 3600.;
-			DistTime = ceil(DistTime / 3600.);
+			nSamples = INT_MAX / 10;
 		}
-		else if (DistTime > 60.)
+
+		int dist = 1;
+		unsigned nTickDist = 1;
+		// find the closest bigger 1,2,5 * 10^x
+		nTickCount = 1;
+
+		for (unsigned k = 1; k <= INT_MAX / 10; k *= 10)
 		{
-			multiplier = 60.;
-			DistTime = ceil(DistTime / 60.);
+			dist = k;
+			nTickDist = k;
+
+			if (dist >= nSamples)
+			{
+				if (dist > 10)
+				{
+					nTickDist = dist / 10;
+					nTickCount = 10;
+				}
+				break;
+			}
+			dist = k * 2;
+			if (dist >= nSamples)
+			{
+				nTickCount = 2;
+				break;
+			}
+			dist = k * 5;
+			if (dist >= nSamples)
+			{
+				nTickCount = 2;
+				break;
+			}
 		}
-		else
+
+		DistTime = dist / SampleRate;
+	}
+		break;
+	case ShowHhMmSs:
+	{
+		// calculate position string length
+
+		nLength = pDC->GetTextExtent(_T("00:00:00.0000"), 13).cx;
+
+		DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
+		// select distance between ticks
+		double multiplier = 1.;
+		double divisor = 1.;
+
+		if (DistTime >= 1.)
+		{
+			if (DistTime > 3600.)
+			{
+				multiplier = 3600.;
+				DistTime = ceil(DistTime / 3600.);
+			}
+			else if (DistTime > 60.)
+			{
+				multiplier = 60.;
+				DistTime = ceil(DistTime / 60.);
+			}
+			else
+			{
+				DistTime = ceil(DistTime);
+			}
+			// find the closest bigger 1,2,5, 10, 20, 30
+			if (DistTime <= 1.)
+			{
+				DistTime = 1.;
+				nTickCount = 10;
+			}
+			else if (DistTime <= 2.)
+			{
+				DistTime = 2.;
+				nTickCount = 2;
+			}
+			else if (DistTime <= 5.)
+			{
+				DistTime = 5.;
+				nTickCount = 5;
+			}
+			else if (DistTime <= 10.)
+			{
+				DistTime = 10.;
+				nTickCount = 10;
+			}
+			else if (DistTime <= 20.)
+			{
+				DistTime = 20.;
+				nTickCount = 2;
+			}
+			else if (DistTime <= 30.)
+			{
+				DistTime = 30.;
+				nTickCount = 3;
+			}
+			else
+			{
+				DistTime = 60.;
+				nTickCount = 6;
+			}
+		}
+		else    // DistTime < 1
+		{
+			divisor = 1000.;
+			DistTime = ceil(DistTime * 1000.);
+			// find the closest bigger 1,2,5 * 10^x
+			for (; ; multiplier *= 10., DistTime = ceil(DistTime / 10.))
+			{
+				if (DistTime <= 1.)
+				{
+					DistTime = 1.;
+					nTickCount = 10;
+					break;
+				}
+				else if (DistTime <= 2.)
+				{
+					DistTime = 2.;
+					nTickCount = 2;
+					break;
+				}
+				else if (DistTime <= 5.)
+				{
+					DistTime = 5.;
+					nTickCount = 5;
+					break;
+				}
+			}
+		}
+
+		DistTime = DistTime * multiplier / divisor;
+	}
+		break;
+
+	case ShowSeconds:
+	{
+		// calculate position string length
+
+		nLength = pDC->GetTextExtent(_T("00,000.0000"), 14).cx;
+
+		DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
+		// select distance between ticks
+		double multiplier = 1.;
+		double divisor = 1.;
+
+		if (DistTime >= 1.)
 		{
 			DistTime = ceil(DistTime);
 		}
-		// find the closest bigger 1,2,5, 10, 20, 30
-		if (DistTime <= 1.)
-		{
-			DistTime = 1.;
-			nTickCount = 10;
-		}
-		else if (DistTime <= 2.)
-		{
-			DistTime = 2.;
-			nTickCount = 2;
-		}
-		else if (DistTime <= 5.)
-		{
-			DistTime = 5.;
-			nTickCount = 5;
-		}
-		else if (DistTime <= 10.)
-		{
-			DistTime = 10.;
-			nTickCount = 10;
-		}
-		else if (DistTime <= 20.)
-		{
-			DistTime = 20.;
-			nTickCount = 2;
-		}
-		else if (DistTime <= 30.)
-		{
-			DistTime = 30.;
-			nTickCount = 3;
-		}
 		else
 		{
-			DistTime = 60.;
-			nTickCount = 6;
+			divisor = 1000.;
+			DistTime = ceil(DistTime * 1000.);
 		}
-	}
-	else    // DistTime < 1
-	{
-		divisor = 1000.;
-		DistTime = ceil(DistTime * 1000.);
+
 		// find the closest bigger 1,2,5 * 10^x
 		for (; ; multiplier *= 10., DistTime = ceil(DistTime / 10.))
 		{
@@ -285,235 +287,106 @@ void CTimeRulerView::DrawRulerHhMmSs(CDC* pDC)
 				break;
 			}
 		}
+
+		DistTime = DistTime * multiplier / divisor;
 	}
+		break;
 
-
-	DistTime = DistTime * multiplier / divisor;
-
-	pDC->SetTextAlign(TA_BOTTOM | TA_LEFT);
-	pDC->SetTextColor(0x000000);   // black
-	pDC->SetBkMode(TRANSPARENT);
-
-	CGdiObjectSave OldPen(pDC, pDC->SelectStockObject(BLACK_PEN));
-
-	pDC->MoveTo(cr.left, cr.bottom - 5);
-	pDC->LineTo(cr.right, cr.bottom - 5);
-
-	pDC->SelectStockObject(WHITE_PEN);
-	pDC->MoveTo(cr.left, cr.bottom - 4);
-	pDC->LineTo(cr.right, cr.bottom - 4);
-
-	pDC->SelectObject( & DarkGrayPen);
-	pDC->MoveTo(cr.left, cr.bottom - 6);
-	pDC->LineTo(cr.right, cr.bottom - 6);
-
-	double nFirstTime = floor(WindowToWorldX(cr.left - nLength) / (SampleRate * DistTime))
-						* DistTime;
-
-	if (nFirstTime < 0)
-	{
-		nFirstTime = 0;
-	}
-
-	for(int nTick = 0; ; nTick++)
-	{
-		double time = nFirstTime + DistTime
-					* nTick / double(nTickCount);
-
-		double sample = time * SampleRate;
-		if (sample > pDoc->WaveFileSamples())
-		{
-			break;
-		}
-
-		int x = WorldToWindowXrnd(sample);
-		if (x > cr.right)
-		{
-			break;
-		}
-
-		pDC->SelectStockObject(BLACK_PEN);
-		pDC->MoveTo(x - 1, cr.bottom - 6);
-
-		if (0 == nTick % nTickCount)
-		{
-			// draw bigger tick (6 pixels high) and the number
-			pDC->LineTo(x - 1, cr.bottom - 12);
-			int flags = TimeToHhMmSs_NeedsHhMm;
-			if (DistTime < 1.)
-			{
-				flags = TimeToHhMmSs_NeedsHhMm | TimeToHhMmSs_NeedsMs;
-			}
-			CString s = TimeToHhMmSs(unsigned((time + 0.0005) * 1000), flags);
-
-			pDC->TextOut(x + 2, cr.bottom - 9, s);
-
-			pDC->SelectStockObject(WHITE_PEN);
-			pDC->MoveTo(x, cr.bottom - 6);
-			pDC->LineTo(x, cr.bottom - 12);
-		}
-		else
-		{
-			// draw small tick (2 pixels high)
-			pDC->LineTo(x - 1, cr.bottom - 8);
-
-			pDC->SelectStockObject(WHITE_PEN);
-			pDC->MoveTo(x, cr.bottom - 6);
-			pDC->LineTo(x, cr.bottom - 8);
-		}
-	}
-
-}
-
-void CTimeRulerView::DrawRulerSeconds(CDC* pDC)
-{
-	CWaveSoapFrontDoc* pDoc = GetDocument();
-	if (! pDoc->m_WavFile.IsOpen())
-	{
+	default:
 		return;
 	}
 
-	// background is erased by gray brush.
-	// draw horizontal line with ticks and numbers
-	CGdiObjectSave OldFont(pDC, pDC->SelectStockObject(ANSI_VAR_FONT));
-
-	CPen DarkGrayPen(PS_SOLID, 0, 0x808080);
-	CRect cr;
-	GetClientRect( & cr);
-
-	// calculate position string length
-	int nTickCount;
-	float SampleRate = float(pDoc->WaveSampleRate());
-
-	int nLength = pDC->GetTextExtent(_T("00,000.0000"), 14).cx;
-
-	double DistTime = 1.5 * nLength / GetXScaleDev() / SampleRate;
-	// select distance between ticks
-	double multiplier = 1.;
-	double divisor = 1.;
-
-	if (DistTime >= 1.)
-	{
-		DistTime = ceil(DistTime);
-	}
-	else
-	{
-		divisor = 1000.;
-		DistTime = ceil(DistTime * 1000.);
-	}
-
-	// find the closest bigger 1,2,5 * 10^x
-	for (; ; multiplier *= 10., DistTime = ceil(DistTime / 10.))
-	{
-		if (DistTime <= 1.)
-		{
-			DistTime = 1.;
-			nTickCount = 10;
-			break;
-		}
-		else if (DistTime <= 2.)
-		{
-			DistTime = 2.;
-			nTickCount = 2;
-			break;
-		}
-		else if (DistTime <= 5.)
-		{
-			DistTime = 5.;
-			nTickCount = 5;
-			break;
-		}
-	}
-
-	DistTime = DistTime * multiplier / divisor;
-
-	pDC->SetTextAlign(TA_BOTTOM | TA_LEFT);
-	pDC->SetTextColor(0x000000);   // black
-	pDC->SetBkMode(TRANSPARENT);
-
-	CGdiObjectSave OldPen(pDC, pDC->SelectStockObject(BLACK_PEN));
-
-	pDC->MoveTo(cr.left, cr.bottom - 5);
-	pDC->LineTo(cr.right, cr.bottom - 5);
-
-	pDC->SelectStockObject(WHITE_PEN);
-	pDC->MoveTo(cr.left, cr.bottom - 4);
-	pDC->LineTo(cr.right, cr.bottom - 4);
-
-	pDC->SelectObject( & DarkGrayPen);
-	pDC->MoveTo(cr.left, cr.bottom - 6);
-	pDC->LineTo(cr.right, cr.bottom - 6);
-
-	TCHAR DecimalPoint = GetApp()->m_DecimalPoint;
-
-	double nFirstTime = floor(WindowToWorldX(cr.left - nLength) / (SampleRate * DistTime))
-						* DistTime;
+	nFirstTime = floor(WindowToWorldX(cr.left - nLength) / (SampleRate * DistTime))
+				* DistTime;
 
 	if (nFirstTime < 0)
 	{
 		nFirstTime = 0;
 	}
 
+	CString s1;
+
 	for(int nTick = 0; ; nTick++)
 	{
 		double time = nFirstTime + DistTime
 					* nTick / double(nTickCount);
 
-		double sample = time * SampleRate;
+		double sample = fround(time * SampleRate);
+
 		if (sample > pDoc->WaveFileSamples())
 		{
 			break;
 		}
 
-		int x = WorldToWindowXrnd(sample);
+		int x = WorldToWindowX(sample);
 
 		if (x > cr.right)
 		{
 			break;
 		}
-		pDC->SelectStockObject(BLACK_PEN);
-		pDC->MoveTo(x - 1, cr.bottom - 6);
 
 		if (0 == nTick % nTickCount)
 		{
 			// draw bigger tick (6 pixels high) and the number
-			pDC->LineTo(x - 1, cr.bottom - 12);
-			CString s;
-			time += 0.0005;
-			int ss = int(time);
-			time -= ss;
-			int ms = int(time * 1000.);
-			CString s1 = LtoaCS(ss);
-
-			if (DistTime < 1.)
-			{
-				s.Format(_T("%s%c%03d"),
-						s1, DecimalPoint,
-						ms);
-			}
-			else
-			{
-				s.Format(_T("%s%c0"),
-						s1, DecimalPoint);
-			}
-
-			pDC->TextOut(x + 2, cr.bottom - 9, s);
-
 			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x + 1, cr.bottom - 6);
+			pDC->LineTo(x + 1, cr.bottom - 12);
+
+			pDC->SelectStockObject(BLACK_PEN);
 			pDC->MoveTo(x, cr.bottom - 6);
 			pDC->LineTo(x, cr.bottom - 12);
+
+			switch (m_CurrentDisplayMode)
+			{
+			case ShowSamples:
+			{
+				s = LtoaCS(SAMPLE_INDEX(sample));
+			}
+				break;
+			case ShowHhMmSs:
+			{
+				int flags = TimeToHhMmSs_NeedsHhMm;
+				if (DistTime < 1.)
+				{
+					flags = TimeToHhMmSs_NeedsHhMm | TimeToHhMmSs_NeedsMs;
+				}
+				s = TimeToHhMmSs(unsigned((time + 0.0005) * 1000), flags);
+			}
+				break;
+			case ShowSeconds:
+			{
+				time += 0.0005;
+				int ss = int(time);
+				time -= ss;
+				int ms = int(time * 1000.);
+
+				s1 = LtoaCS(ss);
+
+				if (DistTime < 1.)
+				{
+					s.Format(_T("%s%c%03d"), s1, DecimalPoint, ms);
+				}
+				else
+				{
+					s.Format(_T("%s%c0"), s1, DecimalPoint);
+				}
+			}
+			}
+			pDC->TextOut(x + 2, cr.bottom - 9, s);
+
 		}
 		else
 		{
 			// draw small tick (2 pixels high)
-			pDC->LineTo(x - 1, cr.bottom - 8);
-
-			pDC->SelectStockObject(WHITE_PEN);
+			pDC->SelectStockObject(BLACK_PEN);
 			pDC->MoveTo(x, cr.bottom - 6);
 			pDC->LineTo(x, cr.bottom - 8);
+
+			pDC->SelectStockObject(WHITE_PEN);
+			pDC->MoveTo(x + 1, cr.bottom - 6);
+			pDC->LineTo(x + 1, cr.bottom - 8);
 		}
 	}
-
 }
 
 /////////////////////////////////////////////////////////////////////////////
