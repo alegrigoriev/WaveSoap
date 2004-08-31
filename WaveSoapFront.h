a5945236abf906faf65f2a55d283d44df18843f6
Alexander Grigoriev <alegr@earthlink.net>
2004-08-30 20:58:32 -0700

    Removed #include resource.h

--- WaveSoapFront.h
+++ WaveSoapFront.h
@@ -13,8 +13,6 @@
 	#error include 'stdafx.h' before including this file for PCH
 #endif
 
-#include "resource.h"       // main symbols
-
 #include "ApplicationProfile.h"
 #include "WaveFile.h"
 #include "KListEntry.h"
