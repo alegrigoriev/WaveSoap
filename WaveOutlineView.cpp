28ad8ce6c86ac26c84a421d5c946169ccf3d08dd
Alexander Grigoriev <alegr@earthlink.net>
2004-09-22 22:00:16 -0700

    Region shorter than one pixel of outline view was not shown

--- WaveOutlineView.cpp
+++ WaveOutlineView.cpp
@@ -258,17 +258,21 @@ void CWaveOutlineView::OnDraw(CDC* pDC)
 	pDC->SetROP2(R2_XORPEN);
 	pDC->SetBkColor(RGB(0, 0, 0));
 
+	long prev_x = -0x80000000;
+
 	for (SAMPLE_INDEX_Vector::const_iterator i = markers.begin();
 		i < markers.end(); i++)
 	{
 		long x = MulDiv( *i, cr.right, nSamples);
 
-		if (x >= ur.left
+		if (x != prev_x
+			&& x >= ur.left
 			&& x < ur.right)
 		{
 			// draw marker
 			pDC->MoveTo(x, cr.top);
 			pDC->LineTo(x, cr.bottom);
+			prev_x = x;
 		}
 	}
 
