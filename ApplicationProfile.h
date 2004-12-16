b7afd31eaa95c1f18d08daa971ceb09d633da6c0
Alexander Grigoriev <alegr@earthlink.net>
2004-12-15 21:23:28 -0800

    ResetSectionToDefault, ResetAllToDefault and ResetSectionToInitial/ResetSectionToDefault functions added
    CApplicationProfileItem and derived classes' declarations moved to the implementation file.

--- ApplicationProfile.h
+++ ApplicationProfile.h
@@ -3,202 +3,9 @@
 #ifndef APPLICATIONPROFILE_H
 #define APPLICATIONPROFILE_H
 #pragma once
+#include "KListEntry.h"
 
-class CApplicationProfile;
-class CApplicationProfileItem
-{
-public:
-	CApplicationProfileItem * Next;
-	virtual void WriteData(BOOL bForceWrite=FALSE) =0;
-	virtual void ReadData() =0;
-	virtual void ResetToDefault() = 0;
-	virtual void ResetToInitial() = 0;
-	CApplicationProfileItem(CApplicationProfile * pProfile, LPCTSTR szSection, LPCTSTR szName);
-	virtual ~CApplicationProfileItem() {}
-	CString Section;
-	CString Name;
-	CApplicationProfile * m_pProfile;
-	BOOL Exists;
-};
-
-class CApplicationProfileItemStr: public CApplicationProfileItem
-{
-	CString & StrRef;
-	CString InitialData;
-	CString m_Default;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemStr(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName,
-								CString& StringReference, LPCTSTR Default);
-};
-
-class CApplicationProfileItemLong: public CApplicationProfileItem
-{
-	LONG & LongRef;
-	LONG InitialData;
-	LONG m_Default;
-	LONG m_MinVal;
-	LONG m_MaxVal;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemLong(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, LONG & RefValue,
-								LONG Default, LONG MinVal, LONG MaxVal);
-};
-
-class CApplicationProfileItemInt: public CApplicationProfileItem
-{
-protected:
-	int & Ref;
-	int InitialData;
-	int m_Default;
-	int m_MinVal;
-	int m_MaxVal;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemInt(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, int & RefValue,
-								int Default, int MinVal, int MaxVal);
-};
-
-class CApplicationProfileItemUint: public CApplicationProfileItem
-{
-protected:
-	unsigned int & Ref;
-	unsigned int InitialData;
-	unsigned int m_Default;
-	unsigned int m_MinVal;
-	unsigned int m_MaxVal;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemUint(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, unsigned int & RefValue,
-								unsigned int Default, unsigned int MinVal, unsigned int MaxVal);
-};
-
-class CApplicationProfileItemShort : public CApplicationProfileItemInt
-{
-	short & ShortRef;
-	int m_TempInt;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemShort(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, short & RefValue,
-								short Default, short MinVal, short MaxVal);
-};
-
-class CApplicationProfileItemUshort : public CApplicationProfileItemUint
-{
-	unsigned short & ShortRef;
-	unsigned int m_TempInt;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemUshort(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, unsigned short & RefValue,
-								unsigned short Default, unsigned short MinVal, unsigned short MaxVal);
-};
-
-template<class T> class CApplicationProfileItemBinary: public CApplicationProfileItem
-{
-	T * m_Ref;
-	T m_InitialData;
-	T m_Default;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault() { memcpy(m_Ref, & m_Default, sizeof T); }
-	virtual void ResetToInitial() { memcpy(m_Ref, & m_InitialData, sizeof T); }
-	CApplicationProfileItemBinary(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, T & RefValue,
-								T const & Default);
-	~CApplicationProfileItemBinary() {}
-};
-
-class CApplicationProfileItemBool: public CApplicationProfileItemInt
-{
-	bool & Ref;
-	int m_TempVal;
-	bool InitialData;
-	bool m_bDefault;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemBool(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, bool & RefValue,
-								bool Default = false);
-};
-
-class CApplicationProfileItemUlong: public CApplicationProfileItem
-{
-	ULONG & LongRef;
-	ULONG InitialData;
-	ULONG m_Default;
-	ULONG m_MinVal;
-	ULONG m_MaxVal;
-public:
-	virtual void WriteData(BOOL bForceWrite);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemUlong(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, ULONG & RefValue,
-								ULONG Default, ULONG MinVal, ULONG MaxVal);
-};
-
-class CApplicationProfileItemDouble: public CApplicationProfileItem
-{
-	double & DoubleRef;
-	double InitialData;
-	double m_Default;
-	double m_MinVal;
-	double m_MaxVal;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemDouble(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, double & RefValue,
-								double Default, double MinVal, double MaxVal);
-	~CApplicationProfileItemDouble() {}
-};
-
-class CApplicationProfileItemFloat: public CApplicationProfileItemDouble
-{
-	float & FloatRef;
-	double InitialData;
-	double IntermediateValue;
-public:
-	virtual void WriteData(BOOL bForceWrite=FALSE);
-	virtual void ReadData();
-	virtual void ResetToDefault();
-	virtual void ResetToInitial();
-	CApplicationProfileItemFloat(CApplicationProfile * pProfile,
-								LPCTSTR szSection, LPCTSTR szName, float & RefValue,
-								double Default, double MinVal, double MaxVal);
-	~CApplicationProfileItemFloat() {}
-};
+class CApplicationProfileItem;
 
 class CApplicationProfile
 {
@@ -233,16 +40,16 @@ public:
 
 	void AddItem(LPCTSTR szSection, LPCTSTR szName, bool & val,
 				bool nDefault = false);
-	// Visual C 6 doesn't like out of class member template definition
+
+	void AddItem(LPCTSTR szSection, LPCTSTR szName,
+				void * pValue, void const * pDefault, size_t size);
+
 	template <class T> void AddItem(LPCTSTR szSection, LPCTSTR szName,
 									T & value, T const & Default)
 	{
-		CApplicationProfileItem * pTmp;
-		RemoveItem(szSection, szName);
-		pTmp = new CApplicationProfileItemBinary<T>(this, szSection, szName, value, Default);
-		pTmp->Next = pItems;
-		pItems = pTmp;
+		AddItem(szSection, szName, & value, & Default, sizeof T);
 	}
+
 	void AddBoolItem(LPCTSTR szSection, LPCTSTR szName, int & val,
 					int nDefault = 0);
 	void AddItem(LPCTSTR szSection, LPCTSTR szName, float & val,
@@ -256,8 +63,18 @@ public:
 	void RemoveSection(LPCTSTR szSection);
 	BOOL ResetItemToDefault(LPCTSTR szSection, LPCTSTR szName);
 	void ResetSectionToDefault(LPCTSTR szSection);
+	void ResetAllToDefault()
+	{
+		ResetSectionToDefault(NULL);
+	}
+
+
 	BOOL RevertItemToInitial(LPCTSTR szSection, LPCTSTR szName);
 	void RevertSectionToInitial(LPCTSTR szSection);
+	void RevertAllToInitial()
+	{
+		RevertSectionToInitial(NULL);
+	}
 
 	// Flush means write all changed data back to profile
 	BOOL FlushItem(LPCTSTR szSection, LPCTSTR szName);
@@ -293,7 +110,8 @@ public:
 	void CloseCachedKeys();
 	LPCTSTR GetProfileName() const;
 
-	CApplicationProfileItem * pItems;
+	ListHead<CApplicationProfileItem> Items;
+
 	HKEY hCachedRegistryKey;
 	HKEY hCachedSectionKey;
 	CString sCachedSectionName;
@@ -301,43 +119,6 @@ public:
 	CString m_pszRegistryKey;
 	CString m_pszAppName;
 };
-
-template<class T>
-CApplicationProfileItemBinary<T>::CApplicationProfileItemBinary(
-	CApplicationProfile * pProfile, LPCTSTR szSection, LPCTSTR szName,
-	T & Reference, T const & Default)
-	: CApplicationProfileItem(pProfile, szSection, szName),
-	m_Ref( & Reference)
-{
-	memcpy( & m_Default, & Default, sizeof T);
-	// read value
-	ReadData();
-	memcpy(& m_InitialData, m_Ref, sizeof T);
-}
-
-template<class T> void CApplicationProfileItemBinary<T>::ReadData()
-{
-	BYTE * pData = NULL;
-	UINT ReadBytes = 0;
-	BOOL res = m_pProfile->GetProfileBinary(Section, Name, & pData, & ReadBytes);
-	if (res && NULL != pData && ReadBytes == sizeof (T))
-	{
-		memcpy(m_Ref, pData, sizeof (T));
-	}
-	else
-	{
-		memcpy(m_Ref, & m_Default, sizeof (T));
-	}
-	delete[] pData;
-}
-
-template<class T> void CApplicationProfileItemBinary<T>::WriteData(BOOL bForceWrite)
-{
-	if (bForceWrite || memcmp(m_Ref, & m_InitialData, sizeof (T)))
-	{
-		m_pProfile->WriteProfileBinary(Section, Name, LPBYTE(m_Ref), sizeof (T));
-	}
-}
 
 class CStringHistory
 {
