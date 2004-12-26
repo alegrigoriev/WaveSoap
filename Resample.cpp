cbfb4216f483464aa782d035cdd4f042c165d37f
Alexander Grigoriev <alegr@earthlink.net>
2004-12-26 13:48:04 -0800

    Use M_PI and other constants from math.h with _USE_MATH_DEFINE

--- Resample.cpp
+++ Resample.cpp
@@ -6,9 +6,8 @@
 #include "Resample.h"
 #include "MessageBoxSynch.h"
 
-#define M_PI        3.14159265358979323846
-#define M_PI_2      1.57079632679489661923
-#define M_PI_4      0.785398163397448309616
+#define _USE_MATH_DEFINES   // for M_PI definition
+#include <math.h>
 
 CResampleContext::CResampleContext(CWaveSoapFrontDoc * pDoc,
 									UINT StatusStringId, UINT OperationNameId,
