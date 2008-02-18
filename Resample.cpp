4d7b12d75e51384efb9c69e75c9bb4725da98a20
Alexander Grigoriev <alegr@earthlink.net>
2008-02-18 11:12:00 -0800

    Derive CResampleContext from CWaveProcContext

--- Resample.cpp
+++ Resample.cpp
@@ -13,141 +13,20 @@ CResampleContext::CResampleContext(CWaveSoapFrontDoc * pDoc,
 									UINT StatusStringId, UINT OperationNameId,
 									CWaveFile & SrcFile, CWaveFile &DstFile,
 									long OriginalSampleRate, long NewSampleRate,
-									int FilterLength)
-	: BaseClass(pDoc, 0, StatusStringId, OperationNameId)
-	, m_Resample(OriginalSampleRate, NewSampleRate, FilterLength, SrcFile.Channels())
+									int FilterLength,
+									BOOL KeepSamplesPerSec)
+	: BaseClass(pDoc, StatusStringId, OperationNameId)
 {
 	InitSource(SrcFile, 0, LAST_SAMPLE, ALL_CHANNELS);
-	InitDestination(DstFile, 0, LAST_SAMPLE, ALL_CHANNELS, FALSE, 0, 0);
-}
-
-BOOL CResampleContext::OperationProc()
-{
-	//SAMPLE_POSITION dwOperationBegin = m_DstPos;
-	DWORD dwStartTime = GetTickCount();
-
-	if (m_Flags & OperationContextStopRequested)
-	{
-		m_Flags |= OperationContextStop;
-		return TRUE;
-	}
-	if (m_DstPos >= m_DstEnd)
-	{
-		m_Flags |= OperationContextFinished;
-		return TRUE;
-	}
-
-	DWORD LeftToRead = 0;
-	DWORD LeftToWrite = 0;
-	DWORD WasRead = 0;
-	DWORD WasLockedToWrite = 0;
-	void * pOriginalSrcBuf = NULL;
-	char * pSrcBuf = NULL;
-	void * pOriginalDstBuf = NULL;
-	char * pDstBuf = NULL;
-
-	do
-	{
-		if (0 == LeftToRead)
-		{
-			MEDIA_FILE_SIZE SizeToRead = m_SrcEnd - m_SrcPos;
-			if (SizeToRead > CDirectFile::CacheBufferSize())
-			{
-				SizeToRead = CDirectFile::CacheBufferSize();
-			}
-			if (SizeToRead != 0)
-			{
-				WasRead = m_SrcFile.GetDataBuffer( & pOriginalSrcBuf,
-													SizeToRead, m_SrcPos, CDirectFile::GetBufferAndPrefetchNext);
-				if (0 == WasRead)
-				{
-					m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-												CDirectFile::ReturnBufferDirty);
-
-					return FALSE;
-				}
-				pSrcBuf = (char *) pOriginalSrcBuf;
-				LeftToRead = WasRead;
-			}
-			else
-			{
-				pSrcBuf = NULL;
-				LeftToRead = 0;
-				WasRead = 0;
-			}
-		}
-
-		if (0 == LeftToWrite)
-		{
-			MEDIA_FILE_SIZE SizeToWrite = m_DstEnd - m_DstPos;
-			if (0 == SizeToWrite)
-			{
-				break;  // all data written
-			}
-			WasLockedToWrite = m_DstFile.GetDataBuffer( & pOriginalDstBuf,
-														SizeToWrite, m_DstPos, CDirectFile::GetBufferWriteOnly);
-
-			if (0 == WasLockedToWrite)
-			{
-				m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-											CDirectFile::ReturnBufferDiscard);
-
-				return FALSE;
-			}
-			pDstBuf = (char *) pOriginalDstBuf;
-			LeftToWrite = WasLockedToWrite;
-
-		}
-
-		size_t SrcBufUsed = 0;
-
-		size_t DstBufUsed = m_Resample.ProcessSound(pSrcBuf, pDstBuf, LeftToRead,
-													LeftToWrite, & SrcBufUsed);
-
-		if (0) TRACE("ResampleContext: SrcPos=%d (0x%X), DstPos=%d (0x%X), src: %d bytes, dst: %d bytes\n",
-					m_SrcPos, m_SrcPos, m_DstPos, m_DstPos,
-					SrcBufUsed, DstBufUsed);
-		LeftToRead -= SrcBufUsed;
-		m_SrcPos += SrcBufUsed;
-		pSrcBuf += SrcBufUsed;
-
-		if (0 == LeftToRead)
-		{
-			m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-										CDirectFile::ReturnBufferDiscard);
-			WasRead = 0;
-		}
-
-		LeftToWrite -= DstBufUsed;
-		m_DstPos += DstBufUsed;
-		pDstBuf += DstBufUsed;
-
-		if (0 == LeftToWrite)
-		{
-			m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-										CDirectFile::ReturnBufferDirty);
-			WasLockedToWrite = 0;
-		}
-
-	}
-	while (m_DstPos < m_DstEnd
-			&& timeGetTime() - dwStartTime < 200
-			);
-
-	m_SrcFile.ReturnDataBuffer(pOriginalSrcBuf, WasRead,
-								CDirectFile::ReturnBufferDiscard);
-
-	m_DstFile.ReturnDataBuffer(pOriginalDstBuf, WasLockedToWrite,
-								CDirectFile::ReturnBufferDirty);
-
-	return TRUE;
+	InitDestination(DstFile, 0, LAST_SAMPLE, ALL_CHANNELS, FALSE);
+	AddWaveProc(new CResampleFilter(OriginalSampleRate, NewSampleRate, FilterLength, SrcFile.Channels(), KeepSamplesPerSec));
 }
 
 void CResampleContext::DeInit()
 {
 	BaseClass::DeInit();
 	// if overflow happened, ask the user if it should continue
-	if ( ! m_Resample.WasClipped())
+	if ( ! WasClipped())
 	{
 		return;
 	}
@@ -167,7 +46,7 @@ void CResampleContext::DeInit()
 
 			CString s;
 			s.Format(IDS_SOUND_CLIPPED, m_pContext->m_pDocument->GetTitle(),
-					int(m_pContext->m_Resample.GetMaxClipped() * (100. / 32678)));
+					int(m_pContext->GetMaxClipped() * (100. / 32678)));
 
 			CString s1;
 			s1.LoadString(IDS_CONTINUE_QUESTION);
