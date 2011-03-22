85dd3110f16dbef91d5c6fa033af5ef512864e86
Alexander Grigoriev <alegrigoriev@gmail.com>
2011-03-21 20:06:15 -0700

    Changed BladeMP3EncDLL.h, BladeMp3Encoder.cpp

--- BladeMP3EncDLL.h
+++ BladeMP3EncDLL.h
@@ -70,26 +70,26 @@ typedef		unsigned long			BE_ERR;
 #define CURRENT_STRUCT_VERSION 1
 #define CURRENT_STRUCT_SIZE sizeof(BE_CONFIG)	// is currently 331 bytes
 
-/* OBSOLETE, VALUES STILL WORK
 typedef enum
 {
-	NORMAL_QUALITY=0,
-	LOW_QUALITY,
-	HIGH_QUALITY,
-	VOICE_QUALITY
-} LAME_QUALTIY_PRESET;
+	LQP_NOPRESET			=-1,
 
-*/
-
-typedef enum
-{
-	LQP_NOPRESET=-1,
-
-	// STANDARD QUALITY PRESETS
-	LQP_NORMAL_QUALITY=0,
-	LQP_LOW_QUALITY,
-	LQP_HIGH_QUALITY,
-	LQP_VOICE_QUALITY,
+	// QUALITY PRESETS
+	LQP_NORMAL_QUALITY		= 0,
+	LQP_LOW_QUALITY			= 1,
+	LQP_HIGH_QUALITY		= 2,
+	LQP_VOICE_QUALITY		= 3,
+	LQP_R3MIX				= 4,
+	LQP_VERYHIGH_QUALITY	= 5,
+	LQP_STANDARD			= 6,
+	LQP_FAST_STANDARD		= 7,
+	LQP_EXTREME				= 8,
+	LQP_FAST_EXTREME		= 9,
+	LQP_INSANE				= 10,
+	LQP_ABR					= 11,
+	LQP_CBR					= 12,
+	LQP_MEDIUM				= 13,
+	LQP_FAST_MEDIUM			= 14,
 
 	// NEW PRESET VALUES
 	LQP_PHONE	=1000,
@@ -103,19 +103,28 @@ typedef enum
 	LQP_CD		=9000,
 	LQP_STUDIO	=10000
 
-} LAME_QUALTIY_PRESET;
+} LAME_QUALITY_PRESET;
 
-
-
-typedef struct BE_CONFIG
+typedef enum
 {
+	VBR_METHOD_NONE			= -1,
+	VBR_METHOD_DEFAULT		=  0,
+	VBR_METHOD_OLD			=  1,
+	VBR_METHOD_NEW			=  2,
+	VBR_METHOD_MTRH			=  3,
+	VBR_METHOD_ABR			=  4
+} VBRMETHOD;
+
+
+
+typedef struct	{
 	DWORD	dwConfig;			// BE_CONFIG_XXXXX
 	// Currently only BE_CONFIG_MP3 is supported
 	union	{
 
 		struct	{
 
-			DWORD	dwSampleRate;	// 48000, 44100 and 32000 allowed
+			DWORD	dwSampleRate;		// 48000, 44100 and 32000 allowed
 			BYTE	byMode;			// BE_MP3_MODE_STEREO, BE_MP3_MODE_DUALCHANNEL, BE_MP3_MODE_MONO
 			WORD	wBitrate;		// 32, 40, 48, 56, 64, 80, 96, 112, 128, 160, 192, 224, 256 and 320 allowed
 			BOOL	bPrivate;
@@ -132,30 +141,36 @@ typedef struct BE_CONFIG
 			DWORD			dwStructSize;
 
 			// BASIC ENCODER SETTINGS
-			DWORD			dwSampleRate;	// SAMPLERATE OF INPUT FILE
-			DWORD			dwReSampleRate;	// DOWNSAMPLERATE, 0=ENCODER DECIDES
-			LONG			nMode;			// BE_MP3_MODE_STEREO, BE_MP3_MODE_DUALCHANNEL, BE_MP3_MODE_MONO
-			DWORD			dwBitrate;		// CBR bitrate, VBR min bitrate
-			DWORD			dwMaxBitrate;	// CBR ignored, VBR Max bitrate
-			LONG			nPreset;		// Quality preset, use one of the settings of the LAME_QUALITY_PRESET enum
-			DWORD			dwMpegVersion;	// FUTURE USE, MPEG-1 OR MPEG-2
-			DWORD			dwPsyModel;		// FUTURE USE, SET TO 0
-			DWORD			dwEmphasis;		// FUTURE USE, SET TO 0
+			DWORD			dwSampleRate;		// SAMPLERATE OF INPUT FILE
+			DWORD			dwReSampleRate;		// DOWNSAMPLERATE, 0=ENCODER DECIDES
+			LONG			nMode;				// BE_MP3_MODE_STEREO, BE_MP3_MODE_DUALCHANNEL, BE_MP3_MODE_MONO
+			DWORD			dwBitrate;			// CBR bitrate, VBR min bitrate
+			DWORD			dwMaxBitrate;		// CBR ignored, VBR Max bitrate
+			LONG			nPreset;			// Quality preset, use one of the settings of the LAME_QUALITY_PRESET enum
+			DWORD			dwMpegVersion;		// FUTURE USE, MPEG-1 OR MPEG-2
+			DWORD			dwPsyModel;			// FUTURE USE, SET TO 0
+			DWORD			dwEmphasis;			// FUTURE USE, SET TO 0
 
 			// BIT STREAM SETTINGS
-			BOOL			bPrivate;		// Set Private Bit (TRUE/FALSE)
-			BOOL			bCRC;			// Insert CRC (TRUE/FALSE)
-			BOOL			bCopyright;		// Set Copyright Bit (TRUE/FALSE)
-			BOOL			bOriginal;		// Set Original Bit (TRUE/FALSE)
+			BOOL			bPrivate;			// Set Private Bit (TRUE/FALSE)
+			BOOL			bCRC;				// Insert CRC (TRUE/FALSE)
+			BOOL			bCopyright;			// Set Copyright Bit (TRUE/FALSE)
+			BOOL			bOriginal;			// Set Original Bit (TRUE/FALSE)
 
 			// VBR STUFF
 			BOOL			bWriteVBRHeader;	// WRITE XING VBR HEADER (TRUE/FALSE)
 			BOOL			bEnableVBR;			// USE VBR ENCODING (TRUE/FALSE)
 			INT				nVBRQuality;		// VBR QUALITY 0..9
 			DWORD			dwVbrAbr_bps;		// Use ABR in stead of nVBRQuality
-			BOOL			bNoRes;				// Disable Bit resorvoir
+			VBRMETHOD		nVbrMethod;
+			BOOL			bNoRes;				// Disable Bit resorvoir (TRUE/FALSE)
 
-			BYTE			btReserved[255-2*sizeof(DWORD)];	// FUTURE USE, SET TO 0
+			// MISC SETTINGS
+			BOOL			bStrictIso;			// Use strict ISO encoding rules (TRUE/FALSE)
+			WORD			nQuality;			// Quality Setting, HIGH BYTE should be NOT LOW byte, otherwhise quality=5
+
+			// FUTURE USE, SET TO 0, align strucutre to 331 bytes
+			BYTE			btReserved[255-4*sizeof(DWORD) - sizeof( WORD )];
 
 		} LHV1;					// LAME header version 1
 
